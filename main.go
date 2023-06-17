e4b5909c69844426816236cd653bfa2e495513a7
jiro <jiro853141@gmail.com>
2023-06-18 03:55:18 +0800

    32

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("tom\tjack")
-	fmt.Println("hello\nworld")
-	fmt.Println("C:\\Users\\unuse\\Desktop\\Golang\\gocode")
-	fmt.Println("tom說\"i love you\"")
-	// \r，以當前行的最前面開始輸出，覆蓋掉以前內容
-	fmt.Println("天龍八赴雪山飛\r張飛")
-	// 練習
-	fmt.Println("姓名\t年齡\t籍貫\t住址\njohn\t12\t河北\t北京")
+	// 定義變數
+	var i int
+	i = 10
+	fmt.Println("i =", i)
 }
