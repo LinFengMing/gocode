504f5d8d3b9f8af37dafd0c70be256bd8c6485ee
jiro <jiro853141@gmail.com>
2023-06-20 02:11:56 +0800

    39

--- main.go
+++ main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
-	var n1 = 100
-	// 查看變數的數據類型
-	fmt.Printf("n1 的類型 %T \n", n1)
-	var n2 int64 = 10
-	// 查看變數的占用字節大小和數據類型
-	fmt.Printf("n2 的類型 %T n2 占用字節大小是 %d \n", n2, unsafe.Sizeof(n2))
-	// 整數變數在使用時，遵守保小不保大的原則，在保證程序正確運行下，盡量使用佔用空間小的數據類型
-	var age byte = 90
-	fmt.Println("age =", age)
+	var price float32 = 89.12
+	fmt.Println("price =", price)
+	var num1 float32 = -0.00089
+	var num2 float64 = -7809666.89
+	fmt.Println("num1 =", num1, "num2 =", num2)
+	// 尾數部分可能丟失，造成精度損失
+	var num3 float32 = -123.0000901
+	var num4 float64 = -123.0000901
+	fmt.Println("num3 =", num3, "num4 =", num4)
 }
