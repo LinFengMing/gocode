5475371f2ba1325fc88638e786e3320e7133ae43
jiro <jiro853141@gmail.com>
2023-06-20 00:43:13 +0800

    38

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	// int8 -128 ~ 127
-	var i int8 = 127
-	fmt.Println("i =", i)
-	// uint8 0 ~ 255
-	var k uint8 = 255
-	fmt.Println("k =", k)
-	// int , unit , rune , byte
-	var a int = 8900
-	fmt.Println("a =", a)
-	var b uint = 1
-	fmt.Println("b =", b)
-	var c rune = 999999999
-	fmt.Println("c =", c)
-	var d byte = 255
-	fmt.Println("d =", d)
+	var n1 = 100
+	// 查看變數的數據類型
+	fmt.Printf("n1 的類型 %T \n", n1)
+	var n2 int64 = 10
+	// 查看變數的占用字節大小和數據類型
+	fmt.Printf("n2 的類型 %T n2 占用字節大小是 %d \n", n2, unsafe.Sizeof(n2))
+	// 整數變數在使用時，遵守保小不保大的原則，在保證程序正確運行下，盡量使用佔用空間小的數據類型
+	var age byte = 90
+	fmt.Println("age =", age)
 }
