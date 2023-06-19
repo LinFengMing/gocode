b86659c8ec3af60f825c8ac42610938d152f8410
jiro <jiro853141@gmail.com>
2023-06-20 02:26:20 +0800

    40

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	var price float32 = 89.12
-	fmt.Println("price =", price)
-	var num1 float32 = -0.00089
-	var num2 float64 = -7809666.89
-	fmt.Println("num1 =", num1, "num2 =", num2)
-	// 尾數部分可能丟失，造成精度損失
-	var num3 float32 = -123.0000901
-	var num4 float64 = -123.0000901
-	fmt.Println("num3 =", num3, "num4 =", num4)
+	var num5 = 1.1
+	fmt.Printf("num5 的數據類型是 %T \n", num5)
+	// 十進制形式
+	num6 := 5.12
+	num7 := .123 // 0.123
+	fmt.Println("num6 =", num6, "num7 =", num7)
+	// 科學技數法形式
+	num8 := 5.1234e2   // 5.1234 * 10 的 2 次方
+	num9 := 5.1234e2   // 5.1234 * 10 的 2 次方
+	num10 := 5.1234e-2 // 5.1234 / 10 的 2 次方
+	fmt.Println("num8 =", num8, "num9 =", num9, "num10 =", num10)
 }
