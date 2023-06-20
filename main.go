fdfb7e0b22f63fa9648e9d9693b4232f80a4eefb
jiro <jiro853141@gmail.com>
2023-06-21 00:42:15 +0800

    44

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
-	// bool 類型只能是 true or false
-	var b = false
-	fmt.Println("b =", b)
-	// bool 類型占用儲存空間是一個字節
-	fmt.Println("b 的佔用空間 =", unsafe.Sizeof(b))
+	// 字串一旦賦值了，字串就不能修改了：在 GO 中字串是不能變的
+	var address string = "北京長城 110 hello world"
+	fmt.Println("address =", address)
 }
