50711863e8fca2412805357848f6d2bf4f5ff69c
jiro <jiro853141@gmail.com>
2023-06-21 00:13:49 +0800

    43

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
-	var c4 int = 22269 // 国
-	fmt.Printf("c4 = %c\n", c4)
-	// 字符類型是可以進行運算的，等於是一個整數，運算時是按照碼值運行
-	var n1 = 10 + 'a' // 10 + 97 = 107
-	fmt.Println("n1 =", n1)
+	// bool 類型只能是 true or false
+	var b = false
+	fmt.Println("b =", b)
+	// bool 類型占用儲存空間是一個字節
+	fmt.Println("b 的佔用空間 =", unsafe.Sizeof(b))
 }
