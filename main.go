7a5edc59d56308c6308bee453e2a35ce747aeaaf
jiro <jiro853141@gmail.com>
2023-06-21 00:52:51 +0800

    45

--- main.go
+++ main.go
@@ -5,7 +5,32 @@ import (
 )
 
 func main() {
-	// 字串一旦賦值了，字串就不能修改了：在 GO 中字串是不能變的
-	var address string = "北京長城 110 hello world"
-	fmt.Println("address =", address)
+	str := "abc\nabc"
+	fmt.Println("str =", str)
+	str2 := `
+	package main
+
+	import (
+		"fmt"
+		"unsafe"
+	)
+
+	func main() {
+		// bool 類型只能是 true or false
+		var b = false
+		fmt.Println("b =", b)
+		// bool 類型占用儲存空間是一個字節
+		fmt.Println("b 的佔用空間 =", unsafe.Sizeof(b))
+	}
+	`
+	fmt.Println("str2 =", str2)
+	// 字串拼接方式
+	var str3 = "hello " + "world"
+	str3 += " haha!"
+	fmt.Println("str3 =", str3)
+	var str4 = "hello " + "world" + "hello " + "world" +
+		"hello " + "world" +
+		"hello " + "world" +
+		"hello " + "world"
+	fmt.Println("str4 =", str4)
 }
