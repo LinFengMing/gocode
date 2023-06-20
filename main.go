acb30db0076502bd1b1f731240f471aaaa37622e
jiro <jiro853141@gmail.com>
2023-06-20 23:56:12 +0800

    42

--- main.go
+++ main.go
@@ -5,13 +5,9 @@ import (
 )
 
 func main() {
-	var c1 byte = 'a'
-	var c2 byte = '0'
-	// 是輸出對應字符的 UTF-8 碼值
-	fmt.Println("c1 =", c1)
-	fmt.Println("c2 =", c2)
-	// 需要格式化輸出
-	fmt.Printf("c1 = %c c2 = %c\n", c1, c2)
-	var c3 int = '北'
-	fmt.Printf("c3 = %c c3 對應的碼值 = %d", c3, c3)
+	var c4 int = 22269 // 国
+	fmt.Printf("c4 = %c\n", c4)
+	// 字符類型是可以進行運算的，等於是一個整數，運算時是按照碼值運行
+	var n1 = 10 + 'a' // 10 + 97 = 107
+	fmt.Println("n1 =", n1)
 }
