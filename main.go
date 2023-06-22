78883d7e7e310f531c5c8d003c85e48fb0a41e56
jiro <jiro853141@gmail.com>
2023-06-22 22:44:02 +0800

    50

--- main.go
+++ main.go
@@ -6,26 +6,18 @@ import (
 )
 
 func main() {
-	var num1 int = 99
-	var num2 float64 = 23.456
-	var b bool = true
-	var myChar byte = 'h'
-	var str string
-	str = fmt.Sprintf("%d", num1)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = fmt.Sprintf("%f", num2)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = fmt.Sprintf("%t", b)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = fmt.Sprintf("%c", myChar)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = strconv.FormatInt(int64(num1), 10)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = strconv.FormatFloat(num2, 'f', 10, 64)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	str = strconv.FormatBool(b)
-	fmt.Printf("str type %T str = %q \n", str, str)
-	var num5 int64 = 4567
-	str = strconv.Itoa(int(num5))
-	fmt.Printf("str type %T str = %q \n", str, str)
+	var str string = "true"
+	var b bool
+	b, _ = strconv.ParseBool(str)
+	fmt.Printf("b type %T b = %v \n", b, b)
+	var str2 string = "1234590"
+	var n1 int64
+	var n2 = int(n1)
+	n1, _ = strconv.ParseInt(str2, 10, 64)
+	fmt.Printf("n1 type %T n1 = %v \n", n1, n1)
+	fmt.Printf("n2 type %T n2 = %v \n", n2, n2)
+	var str3 string = "123.456"
+	var f1 float64
+	f1, _ = strconv.ParseFloat(str3, 64)
+	fmt.Printf("f1 type %T f1 = %v \n", f1, f1)
 }
