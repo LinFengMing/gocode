9a0ebcbf6b2cbc836b8053851830a7ccdacb68c7
jiro <jiro853141@gmail.com>
2023-06-24 03:31:33 +0800

    52

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var str string = "ok"
-	var b bool
-	b, _ = strconv.ParseBool(str)
-	fmt.Printf("b type %T b = %v \n", b, b)
-	var str2 string = "hello"
-	var n int64 = 11
-	n, _ = strconv.ParseInt(str2, 10, 64)
-	fmt.Printf("n type %T n = %v \n", n, n)
+	var i int = 10
+	fmt.Println("i 的地址 =", &i)
+	// ptr 是一個指針變數
+	// ptr 的類型是 *int
+	// ptr 的值是 &i
+	var ptr *int = &i
+	fmt.Printf("ptr = %v \n", ptr)
+	fmt.Printf("ptr 的地址 = %v \n", &ptr)
+	fmt.Printf("ptr 指向的值 = %v \n", *ptr)
 }
