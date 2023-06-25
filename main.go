9973478035481da536fc58216b7c35c720354e68
jiro <jiro853141@gmail.com>
2023-06-26 03:19:11 +0800

    61

--- main.go
+++ main.go
@@ -5,19 +5,13 @@ import (
 )
 
 func main() {
-	// 如果運算的數都是整數，除法只會保留整數部分
-	fmt.Println(10 / 4)
-	// 要保留小數部分，需要有浮點數加入運算
-	fmt.Println(10.0 / 4)
-	// a % b = a - a / b * b
-	fmt.Println("10 % 3 =", 10%3)
-	fmt.Println("-10 % 3 =", -10%3)
-	fmt.Println("10 % -3 =", 10%-3)
-	fmt.Println("-10 % -3 =", -10%-3)
-	// ++ and --
-	var i int = 10
-	i++
-	fmt.Println("i =", i)
-	i--
-	fmt.Println("i =", i)
+
+	// 如果還有97天放假，請問幾個星期又幾天
+	var weeks int = 97 / 7
+	var days int = 97 % 7
+	fmt.Printf("%d 個星期又 %d 天\n", weeks, days)
+	// 攝氏溫度 = 5 / 9 * (華氏溫度 - 100)
+	var fahrenheit float32 = 134.2
+	var celsius float32 = 5.0 / 9 * (fahrenheit - 100)
+	fmt.Printf("%v 度 C 等於 %v 度 F", celsius, fahrenheit)
 }
