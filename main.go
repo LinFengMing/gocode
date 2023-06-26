bd5c8b8c96b8f25974922c0aacfbec7dffbe8169
jiro <jiro853141@gmail.com>
2023-06-26 22:49:25 +0800

    62

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 )
 
 func main() {
-
-	// 如果還有97天放假，請問幾個星期又幾天
-	var weeks int = 97 / 7
-	var days int = 97 % 7
-	fmt.Printf("%d 個星期又 %d 天\n", weeks, days)
-	// 攝氏溫度 = 5 / 9 * (華氏溫度 - 100)
-	var fahrenheit float32 = 134.2
-	var celsius float32 = 5.0 / 9 * (fahrenheit - 100)
-	fmt.Printf("%v 度 C 等於 %v 度 F", celsius, fahrenheit)
+	var n1 int = 9
+	var n2 int = 8
+	fmt.Println(n1 == n2)
+	fmt.Println(n1 != n2)
+	fmt.Println(n1 > n2)
+	fmt.Println(n1 >= n2)
+	fmt.Println(n1 < n2)
+	fmt.Println(n1 <= n2)
+	flag := n1 > n2
+	fmt.Println("flag =", flag)
 }
