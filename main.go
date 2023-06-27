ea246875a05f0f99673bea5cb7323101ad9c179b
jiro <jiro853141@gmail.com>
2023-06-27 23:46:48 +0800

    64

--- main.go
+++ main.go
@@ -4,29 +4,27 @@ import (
 	"fmt"
 )
 
+func test() bool {
+	fmt.Println("test....")
+	return true
+}
+
 func main() {
-	var age int = 40
-	if age > 30 && age < 50 {
-		fmt.Println("ok1")
+	var i int = 10
+
+	if i > 9 && test() {
+		fmt.Println("ok...")
 	}
 
-	if age > 30 && age < 40 {
-		fmt.Println("ok2")
+	if i < 9 && test() {
+		fmt.Println("ok...")
 	}
 
-	if age > 30 || age < 50 {
-		fmt.Println("ok3")
+	if i < 9 || test() {
+		fmt.Println("hello...")
 	}
 
-	if age > 30 || age < 40 {
-		fmt.Println("ok4")
-	}
-
-	if age > 30 {
-		fmt.Println("ok5")
-	}
-
-	if !(age > 30) {
-		fmt.Println("ok6")
+	if i > 9 || test() {
+		fmt.Println("hello...")
 	}
 }
