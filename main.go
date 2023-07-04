c74adce0d7dbd679e9cdbf82d4a750fb53f28606
jiro <jiro853141@gmail.com>
2023-07-05 01:33:21 +0800

    67

--- main.go
+++ main.go
@@ -5,11 +5,35 @@ import (
 )
 
 func main() {
-	// 有兩個變數，a 和 b，要求將其交換，但不使用中間變數，最終打印結果
-	var a int = 10
-	var b int = 20
-	a = a + b
-	b = a - b // b = a + a - b ==> b = a
-	a = a - b // a = a + b - a ==> a = b
-	fmt.Printf("a = %v , b = %v \n", a, b)
+	// & 和 * 的使用
+	a := 100
+	fmt.Println("a 的地址是", &a)
+	var ptr *int = &a
+	fmt.Println("ptr 指向的值是", *ptr)
+	var n int
+	var i int = 10
+	var j int = 12
+	// golang 沒有三元運算符
+	if i > j {
+		n = i
+	} else {
+		n = j
+	}
+	fmt.Println("n =", n)
+	// 取兩個數最大值
+	var n1 int = 10
+	var n2 int = 10
+	var max int
+	if n1 > n2 {
+		max = n1
+	} else {
+		max = n2
+	}
+	fmt.Println("max =", max)
+	// 取三個數最大值
+	var n3 = 45
+	if n3 > max {
+		max = n3
+	}
+	fmt.Println("max =", max)
 }
