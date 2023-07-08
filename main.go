9147ea580b11815b72692d2e1c13e5a6c136d2ad
jiro <jiro853141@gmail.com>
2023-07-09 03:16:17 +0800

    69

--- main.go
+++ main.go
@@ -5,22 +5,13 @@ import (
 )
 
 func main() {
-	var name string
-	var age byte
-	var sal float32
-	var isPass bool
-	// 方式一
-	fmt.Println("請輸入姓名")
-	fmt.Scanln(&name)
-	fmt.Println("請輸入年齡")
-	fmt.Scanln(&age)
-	fmt.Println("請輸入薪水")
-	fmt.Scanln(&sal)
-	fmt.Println("請輸入是否通過考試")
-	fmt.Scanln(&isPass)
-	fmt.Printf("姓名是 %v \n年齡是 %v \n薪水是 %v \n是否通過考試 %v \n", name, age, sal, isPass)
-	// 方式二
-	fmt.Println("請輸入你的姓名，年齡，薪水，是否通過考試，請用空格隔開")
-	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
-	fmt.Printf("姓名是 %v \n年齡是 %v \n薪水是 %v \n是否通過考試 %v \n", name, age, sal, isPass)
+	var i int = 5
+	// 二進制輸出
+	fmt.Printf("i = %b \n", i)
+	// 八進制：0-7，滿8進1，以數字0開頭表示
+	var j int = 011 // 8 + 1 = 9
+	fmt.Println("j =", j)
+	// 十六進制：0-9和A-F，滿16進1，以0x或0X開頭表示
+	var k int = 0x11 // 16 + 1 = 17
+	fmt.Println("k =", k)
 }
