4d9e51bdd5bc0bc35ba26ad8c643d3e8cceacc6d
jiro <jiro853141@gmail.com>
2023-07-11 01:59:40 +0800

    75

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 )
 
 func main() {
-	var i int = 5
-	// 二進制輸出
-	fmt.Printf("i = %b \n", i)
-	// 八進制：0-7，滿8進1，以數字0開頭表示
-	var j int = 011 // 8 + 1 = 9
-	fmt.Println("j =", j)
-	// 十六進制：0-9和A-F，滿16進1，以0x或0X開頭表示
-	var k int = 0x11 // 16 + 1 = 17
-	fmt.Println("k =", k)
+	var a int = 1 >> 2
+	var b int = -1 >> 2
+	var c int = 1 << 2
+	var d int = -1 << 2
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+	fmt.Println("c =", c)
+	fmt.Println("d =", d)
+	fmt.Println(2 & 3)
+	fmt.Println(2 | 3)
+	fmt.Println(2 ^ 3)
+	fmt.Println(-2 ^ 2)
 }
