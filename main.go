83f0d520a0075993b60b638de5551c8e1f4bc3a2
jiro <jiro853141@gmail.com>
2023-07-12 00:56:20 +0800

    78

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 )
 
 func main() {
-	var age int
-	fmt.Println("請輸入年齡：")
-	fmt.Scanln(&age)
-	if age > 18 {
+	// golang 可以在 if 中，直接定義一個變數
+	if age := 20; age > 18 {
 		fmt.Println("你的年齡大於18，要對自己的行為負責！")
 	}
 }
