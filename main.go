ec3e5a0a703ea2770e4d984bd90eac7abd5ca1b2
jiro <jiro853141@gmail.com>
2023-07-18 22:29:07 +0800

    86

--- main.go
+++ main.go
@@ -5,41 +5,25 @@ import (
 )
 
 func main() {
-	var second float64
-	fmt.Println("請輸入秒數：")
-	fmt.Scanln(&second)
-	if second <= 8 {
-		var gender string
-		fmt.Println("請輸入姓別：")
-		fmt.Scanln(&gender)
-		if gender == "男" {
-			fmt.Println("進入男子組的決賽")
-		} else {
-			fmt.Println("進入女子組的決賽")
-		}
-	} else {
-		fmt.Println("out")
-	}
-	var month byte
-	var age byte
-	var price float64 = 60.0
-	fmt.Println("請輸入遊玩的月份：")
-	fmt.Scanln(&month)
-	fmt.Println("請輸入年齡：")
-	fmt.Scanln(&age)
-	if month >= 4 && month <= 10 {
-		if age > 60 {
-			fmt.Printf("%v 月 年齡 %v 票價：%v\n", month, age, price/3)
-		} else if age >= 18 {
-			fmt.Printf("%v 月 年齡 %v 票價：%v\n", month, age, price)
-		} else {
-			fmt.Printf("%v 月 年齡 %v 票價：%v\n", month, age, price/2)
-		}
-	} else {
-		if age >= 18 && age < 60 {
-			fmt.Println("淡季成人票價：40")
-		} else {
-			fmt.Println("淡季兒童老人票價：20")
-		}
+	var key byte
+	fmt.Println("請輸入一個字母 a,b,c,d,e,f,g")
+	fmt.Scanf("%c", &key)
+	switch key {
+	case 'a':
+		fmt.Println("星期一")
+	case 'b':
+		fmt.Println("星期二")
+	case 'c':
+		fmt.Println("星期三")
+	case 'd':
+		fmt.Println("星期四")
+	case 'e':
+		fmt.Println("星期五")
+	case 'f':
+		fmt.Println("星期六")
+	case 'g':
+		fmt.Println("星期日")
+	default:
+		fmt.Println("輸入有誤")
 	}
 }
