469fd83955384a5865ab17576a8d6dc61e6618b2
jiro <jiro853141@gmail.com>
2023-07-19 02:20:18 +0800

    88

--- main.go
+++ main.go
@@ -4,38 +4,65 @@ import (
 	"fmt"
 )
 
-func test(char byte) byte {
-	return char + 1
-}
-
 func main() {
-	var key byte
-	fmt.Println("請輸入一個字母 a,b,c,d,e,f,g")
-	fmt.Scanf("%c", &key)
-	switch test(key) {
-	case 'a':
-		fmt.Println("星期一")
-	case 'b':
-		fmt.Println("星期二")
-	case 'c':
-		fmt.Println("星期三")
-	case 'd':
-		fmt.Println("星期四")
-	case 'e':
-		fmt.Println("星期五")
-	case 'f':
-		fmt.Println("星期六")
-	case 'g':
-		fmt.Println("星期日")
+	var age int = 10
+	switch {
+	case age == 10:
+		fmt.Println("age == 10")
+	case age == 20:
+		fmt.Println("age == 20")
 	default:
-		fmt.Println("輸入有誤")
+		fmt.Println("沒有配對到")
 	}
-	var n1 int32 = 20
-	var n2 int32 = 20
-	switch n1 {
-	case n2, 10, 5:
-		fmt.Println("ok")
+	var score int = 90
+	switch {
+	case score > 90:
+		fmt.Println("成績優秀")
+	case score >= 70 && score <= 90:
+		fmt.Println("成績優良")
+	case score >= 60 && score < 70:
+		fmt.Println("成績及格")
 	default:
-		fmt.Println("沒有一致")
+		fmt.Println("成績不及格")
+	}
+	switch grade := 60; {
+	case grade > 90:
+		fmt.Println("成績優秀")
+	case grade >= 70 && grade <= 90:
+		fmt.Println("成績優良")
+	case grade >= 60 && grade < 70:
+		fmt.Println("成績及格")
+	default:
+		fmt.Println("成績不及格")
+	}
+	var num int = 10
+	switch num {
+	case 10:
+		fmt.Println("ok1")
+		fallthrough
+	case 20:
+		fmt.Println("ok2")
+		fallthrough
+	case 30:
+		fmt.Println("ok3")
+	default:
+		fmt.Println("沒有配對到")
+	}
+	var x interface{}
+	var y = 10.0
+	x = y
+	switch i := x.(type) {
+	case nil:
+		fmt.Printf("x 的類型是 %T", i)
+	case int:
+		fmt.Printf("x 的類型是 int")
+	case float64:
+		fmt.Printf("x 的類型是 float64")
+	case func(int) float64:
+		fmt.Printf("x 的類型是 fun(int)")
+	case bool, string:
+		fmt.Printf("x 的類型是 bool 或 string")
+	default:
+		fmt.Printf("x 的類型是未知")
 	}
 }
