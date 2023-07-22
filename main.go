3d136467d222c5ae058557860b0c9f9943826379
jiro <jiro853141@gmail.com>
2023-07-23 00:54:18 +0800

    92

--- main.go
+++ main.go
@@ -8,4 +8,18 @@ func main() {
 	for i := 1; i < 11; i++ {
 		fmt.Println("你好", i)
 	}
+	j := 1
+	for j < 11 {
+		fmt.Println("你好~", j)
+		j++
+	}
+	k := 1
+	for {
+		if k < 11 {
+			fmt.Println("ok")
+		} else {
+			break
+		}
+		k++
+	}
 }
