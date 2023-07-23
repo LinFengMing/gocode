5473f1fd0d68cbaaa2b5d88a33cea3be96e45ca5
jiro <jiro853141@gmail.com>
2023-07-23 23:13:21 +0800

    95

--- main.go
+++ main.go
@@ -5,19 +5,22 @@ import (
 )
 
 func main() {
-	// 印出 1 ~ 100 之間所有9的倍數的整數的個數和總和
-	var count uint64 = 0
-	var sum uint64 = 0
-	var i uint64 = 1
-	for ; i < 101; i++ {
-		if i%9 == 0 {
-			count++
-			sum += i
+	// while
+	var i int = 1
+	for {
+		if i > 10 {
+			break
 		}
+		fmt.Println("hello,world!", i)
+		i++
 	}
-	fmt.Printf("個數 = %v, 總和 = %v\n", count, sum)
-	var n int = 6
-	for i := 0; i <= n; i++ {
-		fmt.Printf("%v + %v = %v\n", i, n-i, n)
+	// do...while
+	var j int = 1
+	for {
+		fmt.Println("hello,world!", j)
+		j++
+		if j > 10 {
+			break
+		}
 	}
 }
