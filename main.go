3b0dc0be99b63d6095b3315ce5b301e93ffa426e
jiro <jiro853141@gmail.com>
2023-08-02 01:06:29 +0800

    96

--- main.go
+++ main.go
@@ -5,22 +5,24 @@ import (
 )
 
 func main() {
-	// while
-	var i int = 1
-	for {
-		if i > 10 {
-			break
+	var classNum int = 3
+	var stuNum int = 5
+	var totalSum float64 = 0.0
+	var passCount int = 0
+	for j := 1; j <= classNum; j++ {
+		var sum float64 = 0.0
+		for i := 1; i <= stuNum; i++ {
+			var score float64
+			fmt.Printf("輸入第%d班第%d個學生的成績\n", j, i)
+			fmt.Scanln(&score)
+			sum += score
+			if score >= 60 {
+				passCount++
+			}
 		}
-		fmt.Println("hello,world!", i)
-		i++
+		fmt.Printf("第%d個班級的平均分是%v\n", j, sum/float64(stuNum))
+		totalSum += sum
 	}
-	// do...while
-	var j int = 1
-	for {
-		fmt.Println("hello,world!", j)
-		j++
-		if j > 10 {
-			break
-		}
-	}
+	fmt.Printf("各個班級的總成績是%v，所有班級平均分是%v\n", totalSum, totalSum/float64(classNum*stuNum))
+	fmt.Printf("及格人數為%v\n", passCount)
 }
