10b85f5cb2e7f8113147aa8773b56b94d9e38392
jiro <jiro853141@gmail.com>
2023-08-04 02:34:40 +0800

    98

--- main.go
+++ main.go
@@ -5,19 +5,10 @@ import (
 )
 
 func main() {
-	var layer int
-	fmt.Println("輸入層數")
-	fmt.Scanln(&layer)
-	for i := 1; i <= layer; i++ {
-		for k := 1; k <= layer-i; k++ {
-			fmt.Print(" ")
-		}
-		for j := 1; j <= 2*i-1; j++ {
-			if j == 1 || j == 2*i-1 || i == layer {
-				fmt.Print("*")
-			} else {
-				fmt.Print(" ")
-			}
+	var number int = 9
+	for i := 1; i <= number; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Printf("%v * %v = %v \t", j, i, j*i)
 		}
 		fmt.Println()
 	}
