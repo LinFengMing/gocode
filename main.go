312c7db738c2da2d1eb2edf5fc3c7bddc6b025f9
jiro <jiro853141@gmail.com>
2023-08-06 02:55:54 +0800

    103

--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 )
 
 func main() {
-lable1:
-	for i := 0; i < 4; i++ {
-		for j := 0; j < 10; j++ {
-			if j == 2 {
-				break lable1
-			}
-			fmt.Println("j =", j)
+	var sum int = 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+		if sum > 20 {
+			fmt.Println("當 sum > 20 時，當前數是 ", i)
+			break
 		}
+	}
+	var name string
+	var pwd string
+	var loginChance int = 3
+	for i := 1; i <= 3; i++ {
+		fmt.Println("請輸入用戶名")
+		fmt.Scanln(&name)
+		fmt.Println("請輸入密碼")
+		fmt.Scanln(&pwd)
+		if name == "jiro" && pwd == "888" {
+			fmt.Println("登陸成功")
+			break
+		} else {
+			loginChance--
+			if loginChance > 0 {
+				fmt.Printf("還有%v次登陸機會\n", loginChance)
+			}
+		}
+	}
+	if loginChance == 0 {
+		fmt.Println("沒有登陸成功")
 	}
 }
