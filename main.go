d2ac58486f96ead816f5e57500ef5330fd4d2bc8
jiro <jiro853141@gmail.com>
2023-08-06 00:51:37 +0800

    102

--- main.go
+++ main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	var count int = 0
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	for {
-		n := rand.Intn(100) + 1
-		fmt.Println(n)
-		count++
-		if n == 99 {
-			break
+lable1:
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				break lable1
+			}
+			fmt.Println("j =", j)
 		}
 	}
-	fmt.Println("生成 99 一共使用了", count, "次")
 }
