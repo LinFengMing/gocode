8a2af1fd332d6322b98275a01bc00f57678b4137
jiro <jiro853141@gmail.com>
2023-08-10 22:43:49 +0800

    107

--- main.go
+++ main.go
@@ -5,35 +5,27 @@ import (
 )
 
 func main() {
-here:
-	for i := 0; i < 2; i++ {
-		for j := 1; j < 4; j++ {
-			if j == 2 {
-				continue here
-			}
-			fmt.Println("i =", i, "j =", j)
-		}
+	var n int = 10
+	fmt.Println("ok1")
+	if n > 20 {
+		goto label
 	}
-	for k := 1; k <= 100; k++ {
-		if k%2 == 0 {
-			continue
-		}
-		fmt.Println("奇數是", k)
+	fmt.Println("ok2")
+	fmt.Println("ok3")
+	fmt.Println("ok4")
+label:
+	fmt.Println("ok5")
+	fmt.Println("ok6")
+	fmt.Println("ok7")
+	var m int = 30
+	fmt.Println("ok1")
+	if m > 20 {
+		return
 	}
-	var positiveCount int
-	var negativeCount int
-	var num int
-	for {
-		fmt.Println("請輸入一個整數")
-		fmt.Scanln(&num)
-		if num == 0 {
-			break
-		}
-		if num > 0 {
-			positiveCount++
-			continue
-		}
-		negativeCount++
-	}
-	fmt.Printf("正數個數是%v, 負數個數是%v", positiveCount, negativeCount)
+	fmt.Println("ok2")
+	fmt.Println("ok3")
+	fmt.Println("ok4")
+	fmt.Println("ok5")
+	fmt.Println("ok6")
+	fmt.Println("ok7")
 }
