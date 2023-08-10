b05b86e36b8c603d8a00b49c921ca96525b0a353
jiro <jiro853141@gmail.com>
2023-08-11 01:59:57 +0800

    109

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 )
 
-func main() {
-	var n1 float64 = 1.2
-	var n2 float64 = 1.3
-	var operator byte = '+'
+func cal(n1 float64, n2 float64, operator byte) float64 {
 	var res float64
 	switch operator {
 	case '+':
@@ -21,21 +18,18 @@ func main() {
 	default:
 		fmt.Println("操作符號錯誤")
 	}
-	fmt.Println("res=", res)
+	return res
+}
+
+func main() {
+	var n1 float64 = 1.2
+	var n2 float64 = 1.3
+	var operator byte = '+'
+	result := cal(n1, n2, operator)
+	fmt.Println("res=", result)
 	n1 = 1.6
 	n2 = 1.5
 	operator = '-'
-	switch operator {
-	case '+':
-		res = n1 + n2
-	case '-':
-		res = n1 - n2
-	case '*':
-		res = n1 * n2
-	case '/':
-		res = n1 / n2
-	default:
-		fmt.Println("操作符號錯誤")
-	}
-	fmt.Println("res=", res)
+	result = cal(n1, n2, operator)
+	fmt.Println("res=", result)
 }
