551faac77570f6f74e1db3d57349bc3055ce3520
jiro <jiro853141@gmail.com>
2023-08-10 23:08:49 +0800

    108

--- main.go
+++ main.go
@@ -5,27 +5,37 @@ import (
 )
 
 func main() {
-	var n int = 10
-	fmt.Println("ok1")
-	if n > 20 {
-		goto label
+	var n1 float64 = 1.2
+	var n2 float64 = 1.3
+	var operator byte = '+'
+	var res float64
+	switch operator {
+	case '+':
+		res = n1 + n2
+	case '-':
+		res = n1 - n2
+	case '*':
+		res = n1 * n2
+	case '/':
+		res = n1 / n2
+	default:
+		fmt.Println("操作符號錯誤")
 	}
-	fmt.Println("ok2")
-	fmt.Println("ok3")
-	fmt.Println("ok4")
-label:
-	fmt.Println("ok5")
-	fmt.Println("ok6")
-	fmt.Println("ok7")
-	var m int = 30
-	fmt.Println("ok1")
-	if m > 20 {
-		return
+	fmt.Println("res=", res)
+	n1 = 1.6
+	n2 = 1.5
+	operator = '-'
+	switch operator {
+	case '+':
+		res = n1 + n2
+	case '-':
+		res = n1 - n2
+	case '*':
+		res = n1 * n2
+	case '/':
+		res = n1 / n2
+	default:
+		fmt.Println("操作符號錯誤")
 	}
-	fmt.Println("ok2")
-	fmt.Println("ok3")
-	fmt.Println("ok4")
-	fmt.Println("ok5")
-	fmt.Println("ok6")
-	fmt.Println("ok7")
+	fmt.Println("res=", res)
 }
