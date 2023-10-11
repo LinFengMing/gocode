6ccaced717d08a50a3a28f9ef924ae5815e3a8ff
jiro <jiro853141@gmail.com>
2023-10-11 20:29:35 +0800

    119

--- main.go
+++ main.go
@@ -4,18 +4,42 @@ import (
 	"fmt"
 )
 
-func peach(n int) int {
-	if n > 10 || n < 1 {
-		fmt.Println("輸入天數不對")
-		return 0
-	}
-	if n == 10 {
-		return 1
-	} else {
-		return (peach(n+1) + 1) * 2
-	}
+type myFunType func(int, int) int
+
+func getSum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
+func myFun(funvar func(int, int) int, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+
+func myFun2(funvar myFunType, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+
+func getSumAndSub(n1 int, n2 int) (sum int, sub int) {
+	sum = n1 + n2
+	sub = n1 - n2
+	return
 }
 
 func main() {
-	fmt.Println("第1天桃子的數量是", peach(1))
+	a := getSum
+	fmt.Printf("a 的類型 %T, getSum 是 %T\n", a, getSum)
+	result := a(10, 40)
+	fmt.Println("result =", result)
+	result2 := myFun(getSum, 50, 60)
+	fmt.Println("result2 =", result2)
+	type myInt int // 在 go 中 myInt 和 int 雖然都是 int 類型，但是 go 認為 myInt 和 int 是兩個類型
+	var num1 myInt
+	var num2 int
+	num1 = 40
+	fmt.Println("num1 =", num1)
+	num2 = int(num1)
+	fmt.Println("num2 =", num2)
+	result3 := myFun2(getSum, 50, 60)
+	fmt.Println("result3 =", result3)
+	c, d := getSumAndSub(1, 2)
+	fmt.Printf("a = %v, b = %v\n", c, d)
 }
