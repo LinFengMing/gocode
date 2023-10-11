a24fad7e4355330b71cd3a947d9329531c35dd3e
jiro <jiro853141@gmail.com>
2023-10-11 13:57:25 +0800

    117

--- main.go
+++ main.go
@@ -4,27 +4,18 @@ import (
 	"fmt"
 )
 
-func fbn(n int) int {
-	if n == 1 || n == 2 {
+func peach(n int) int {
+	if n > 10 || n < 1 {
+		fmt.Println("輸入天數不對")
+		return 0
+	}
+	if n == 10 {
 		return 1
 	} else {
-		return fbn(n-1) + fbn(n-2)
-	}
-}
-
-func f(n int) int {
-	if n == 1 {
-		return 3
-	} else {
-		return 2*f(n-1) + 1
+		return (peach(n+1) + 1) * 2
 	}
 }
 
 func main() {
-	fmt.Println("result =", fbn(3))
-	fmt.Println("result =", fbn(4))
-	fmt.Println("result =", fbn(5))
-	fmt.Println("result =", fbn(6))
-	fmt.Println("result =", f(1))
-	fmt.Println("result =", f(5))
+	fmt.Println("第1天桃子的數量是", peach(1))
 }
