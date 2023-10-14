1a6a8255acbecff38efb9cf32dc1936fbcabb29a
jiro <jiro853141@gmail.com>
2023-10-14 16:40:13 +0800

    121

--- main.go
+++ main.go
@@ -4,23 +4,39 @@ import (
 	"fmt"
 )
 
-func cal(n1 int, n2 int) (sum int, sub int) {
-	sum = n1 + n2
-	sub = n1 - n2
-	return
+type mySum func(int, int) int
+
+func sumOne(n1, n2 float32) float32 {
+	fmt.Printf("n1 type = %T\n", n1)
+	return n1 + n2
 }
 
-func sum(n1 int, args ...int) int {
-	sum := n1
-	for i := 0; i < len(args); i++ {
-		sum += args[i]
-	}
-	return sum
+func sum(n1, n2 int) int {
+	return n1 + n2
+}
+
+// func sum2(n1, n2, n3 int) int {
+// 	return n1 + n2
+// }
+
+func myFunc(funcVar mySum, num1 int, num2 int) int {
+	return funcVar(num1, num2)
+}
+
+func swap(n1 *int, n2 *int) {
+	t := *n1
+	*n1 = *n2
+	*n2 = t
 }
 
 func main() {
-	result1, _ := cal(10, 20)
-	fmt.Printf("result1 = %d\n", result1)
-	result2 := sum(10, 0, -1, 90, 10, 100)
-	fmt.Printf("result2 = %d\n", result2)
+	fmt.Println("sum =", sumOne(1, 2))
+	a := sum
+	// b := sum2
+	fmt.Println(myFunc(a, 1, 2))
+	// fmt.Println(myFunc(b, 1, 2)) // 錯誤，原因是類型不符合，因為不能將 sum2 賦於 mySum
+	c := 10
+	d := 20
+	swap(&c, &d)
+	fmt.Printf("c = %v, b = %v", c, d)
 }
