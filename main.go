42204fcf899e1bb73d1d9af2e6f7fe209abc3f79
jiro <jiro853141@gmail.com>
2023-10-15 21:55:03 +0800

    124

--- main.go
+++ main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-var (
-	Fun1 = func(n1 int, n2 int) int {
-		return n1 * n2
+func AddUpper() func(int) int {
+	var n int = 10
+	var str = "hello"
+	return func(x int) int {
+		n = n + x
+		str += strconv.Itoa(x)
+		fmt.Println("str =", str)
+		return n
 	}
-)
+}
 
 func main() {
-	result := func(n1 int, n2 int) int {
-		return n1 + n2
-	}(10, 20)
-	fmt.Println("result =", result)
-	a := func(n1 int, n2 int) int {
-		return n1 - n2
-	}
-	result2 := a(10, 20)
-	fmt.Println("result2 =", result2)
-	result3 := Fun1(4, 9)
-	fmt.Println("result3 =", result3)
+	f := AddUpper()
+	fmt.Println(f(1))
+	fmt.Println(f(2))
+	fmt.Println(f(3))
 }
