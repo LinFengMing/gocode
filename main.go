ebefaeea5365a8b3db3f71f8be7cfd412bedfc4f
jiro <jiro853141@gmail.com>
2023-10-15 19:55:44 +0800

    123

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
-	"gocode/utils"
 )
 
-var age = test()
-
-func test() int {
-	fmt.Println("test()...")
-	return 90
-}
-
-func init() {
-	fmt.Println("init()...")
-}
+var (
+	Fun1 = func(n1 int, n2 int) int {
+		return n1 * n2
+	}
+)
 
 func main() {
-	fmt.Println("main()...age =", age)
-	fmt.Println("Age =", utils.Age, ", Name =", utils.Name)
+	result := func(n1 int, n2 int) int {
+		return n1 + n2
+	}(10, 20)
+	fmt.Println("result =", result)
+	a := func(n1 int, n2 int) int {
+		return n1 - n2
+	}
+	result2 := a(10, 20)
+	fmt.Println("result2 =", result2)
+	result3 := Fun1(4, 9)
+	fmt.Println("result3 =", result3)
 }
