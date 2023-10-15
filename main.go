eed7cf59e3a32ab66d78762243e067cb75c84f8c
jiro <jiro853141@gmail.com>
2023-10-16 00:17:46 +0800

    125

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
-func AddUpper() func(int) int {
-	var n int = 10
-	var str = "hello"
-	return func(x int) int {
-		n = n + x
-		str += strconv.Itoa(x)
-		fmt.Println("str =", str)
-		return n
+func makeSuffix(suffix string) func(string) string {
+	return func(name string) string {
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		}
+		return name
 	}
 }
 
 func main() {
-	f := AddUpper()
-	fmt.Println(f(1))
-	fmt.Println(f(2))
-	fmt.Println(f(3))
+	f := makeSuffix(".jpg")
+	fmt.Println("檔案名處理後 =", f("winter"))
+	fmt.Println("檔案名處理後 =", f("bird.jpg"))
 }
