f9ef22d7aa6a2ed11f31a60e2bbe870a9663d41b
jiro <jiro853141@gmail.com>
2023-10-16 20:59:11 +0800

    126

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func makeSuffix(suffix string) func(string) string {
-	return func(name string) string {
-		if !strings.HasSuffix(name, suffix) {
-			return name + suffix
-		}
-		return name
-	}
+func sum(n1 int, n2 int) int {
+	// 當執行到 defer，暫時不執行，會將 defer 後面的語法放到 defer 的 stack
+	// 當函式執行完後，再從 defer 的 stack，按照後進先出的方式出 stack 執行
+	defer fmt.Println("ok1 n1 =", n1)
+	defer fmt.Println("ok2 n2 =", n2)
+	result := n1 + n2
+	fmt.Println("ok3 result =", result)
+	return result
 }
 
 func main() {
-	f := makeSuffix(".jpg")
-	fmt.Println("檔案名處理後 =", f("winter"))
-	fmt.Println("檔案名處理後 =", f("bird.jpg"))
+	result := sum(10, 20)
+	fmt.Println("result =", result)
 }
