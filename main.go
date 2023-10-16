5a2d20452720b393026b1112d843c8a83484892c
jiro <jiro853141@gmail.com>
2023-10-17 00:22:47 +0800

    129

--- main.go
+++ main.go
@@ -4,19 +4,26 @@ import (
 	"fmt"
 )
 
-func sum(n1 int, n2 int) int {
-	// 當執行到 defer，暫時不執行，會將 defer 後面的語法放到 defer 的 stack
-	// 當函式執行完後，再從 defer 的 stack，按照後進先出的方式出 stack 執行
-	defer fmt.Println("ok1 n1 =", n1)
-	defer fmt.Println("ok2 n2 =", n2)
-	n1++
-	n2++
-	result := n1 + n2
-	fmt.Println("ok3 result =", result)
-	return result
+var age int = 50
+var Name string = "Jack"
+
+func test() {
+	age := 10
+	Name := "Tom"
+	fmt.Println("age =", age)
+	fmt.Println("Name =", Name)
 }
 
 func main() {
-	result := sum(10, 20)
-	fmt.Println("result =", result)
+	test()
+	fmt.Println("age =", age)
+	fmt.Println("Name =", Name)
+	for i := 0; i < 10; i++ {
+		fmt.Println("i =", i)
+	}
+	var i int
+	for i = 0; i < 10; i++ {
+		fmt.Println("i =", i)
+	}
+	fmt.Println("i =", i)
 }
