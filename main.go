b3994ffa4e3251cdaf5d73cc98461bde97476f5c
jiro <jiro853141@gmail.com>
2023-10-16 22:03:54 +0800

    127

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func sum(n1 int, n2 int) int {
 	// 當函式執行完後，再從 defer 的 stack，按照後進先出的方式出 stack 執行
 	defer fmt.Println("ok1 n1 =", n1)
 	defer fmt.Println("ok2 n2 =", n2)
+	n1++
+	n2++
 	result := n1 + n2
 	fmt.Println("ok3 result =", result)
 	return result
