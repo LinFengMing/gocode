6396bbb7ee4177f961c32cda07c2c12d3803837e
jiro <jiro853141@gmail.com>
2023-10-18 00:27:16 +0800

    131

--- main.go
+++ main.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 )
 
-var name string = "Tome"
-
-func test01() {
-	fmt.Println(name)
+func printPyramid(totalLevel int) {
+	for i := 1; i <= totalLevel; i++ {
+		for k := 1; k <= totalLevel-i; k++ {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= 2*i-1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
 }
-func test02() {
-	name := "Jack"
-	fmt.Println(name)
+
+func printMult(num int) {
+	for i := 1; i <= num; i++ {
+		for j := 1; j <= 1; j++ {
+			fmt.Printf("%v * %v = %v", j, i, j*i)
+		}
+		fmt.Println()
+	}
 }
 
 func main() {
-	fmt.Println(name)
-	test01()
-	test02()
-	test01()
+	var n int
+	fmt.Println("請輸入打印金字塔的層數")
+	fmt.Scanln(&n)
+	printPyramid(n)
+	var num int
+	fmt.Println("請輸入九九表的對應數")
+	fmt.Scanln(&num)
+	printMult(num)
 }
