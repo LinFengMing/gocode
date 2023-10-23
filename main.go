44a554ae204405dc88e58dfecba2f16ad4e4737d
jiro <jiro853141@gmail.com>
2023-10-24 02:00:47 +0800

    135

--- main.go
+++ main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
 func main() {
-	// 字串去除頭尾空格
-	str := strings.TrimSpace(" hello haha ")
-	fmt.Printf("str = %q\n", str)
-	// 字串頭尾去除字串
-	str2 := strings.Trim("! hello! ", " !")
-	fmt.Printf("str2 = %q\n", str2)
-	// 字串頭去除字串
-	str3 := strings.TrimLeft("!hello!", "!")
-	fmt.Printf("str3 = %q\n", str3)
-	str4 := strings.TrimRight("!hello!", "!")
-	fmt.Printf("str4 = %q\n", str4)
-	// 判斷字串是否用某字串開始
-	b := strings.HasPrefix("ftp://192.168.10.1", "ftp")
-	fmt.Printf("b = %v\n", b)
-	// 判斷字串是否用某字串結束
-	b2 := strings.HasSuffix("NLT_abc.jpg", "abc")
-	fmt.Printf("b2 = %v\n", b2)
+	// 取得當前時間
+	now := time.Now()
+	fmt.Printf("now = %v, type = %T\n", now, now)
+	fmt.Printf("年 = %v, \n", now.Year())
+	fmt.Printf("月 = %v, \n", int(now.Month()))
+	fmt.Printf("日 = %v, \n", now.Day())
+	fmt.Printf("時 = %v, \n", now.Hour())
+	fmt.Printf("分 = %v, \n", now.Minute())
+	fmt.Printf("秒 = %v, \n", now.Second())
+	// 格式化日期時間
+	dateStr := fmt.Sprintf("%02d-%02d-%02d %02d:%02d:%02d\n", now.Year(),
+		int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second())
+	fmt.Printf("dateStr = %v\n", dateStr)
+	fmt.Println(now.Format("2006-01-02 15:04:05"))
 }
