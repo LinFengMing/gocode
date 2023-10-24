5d0fde0b57fc3ad758f29695b7d146d882154f4e
jiro <jiro853141@gmail.com>
2023-10-24 21:18:43 +0800

    136

--- main.go
+++ main.go
@@ -6,18 +6,26 @@ import (
 )
 
 func main() {
-	// 取得當前時間
+	// 每 1 秒列印一個數字
+	i := 0
+	for {
+		i++
+		fmt.Println(i)
+		time.Sleep(time.Second)
+		if i == 10 {
+			break
+		}
+	}
+	// 每 0.1 秒列印一個數字
+	j := 0
+	for {
+		j++
+		fmt.Println(j)
+		time.Sleep(time.Millisecond * 100)
+		if j == 10 {
+			break
+		}
+	}
 	now := time.Now()
-	fmt.Printf("now = %v, type = %T\n", now, now)
-	fmt.Printf("年 = %v, \n", now.Year())
-	fmt.Printf("月 = %v, \n", int(now.Month()))
-	fmt.Printf("日 = %v, \n", now.Day())
-	fmt.Printf("時 = %v, \n", now.Hour())
-	fmt.Printf("分 = %v, \n", now.Minute())
-	fmt.Printf("秒 = %v, \n", now.Second())
-	// 格式化日期時間
-	dateStr := fmt.Sprintf("%02d-%02d-%02d %02d:%02d:%02d\n", now.Year(),
-		int(now.Month()), now.Day(), now.Hour(), now.Minute(), now.Second())
-	fmt.Printf("dateStr = %v\n", dateStr)
-	fmt.Println(now.Format("2006-01-02 15:04:05"))
+	fmt.Printf("unix = %v, unixnano = %v", now.Unix(), now.UnixNano())
 }
