daf7f1a9995a7a38fb424c2ba910b107c40aff4a
jiro <jiro853141@gmail.com>
2023-10-27 00:28:57 +0800

    139

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 )
 
+func test() {
+	defer func() {
+		err := recover() // recover() 可以抓取異常
+		if err != nil {
+			fmt.Println("err =", err)
+		}
+	}()
+	num1 := 10
+	num2 := 0
+	result := num1 / num2
+	fmt.Println("result =", result)
+}
+
 func main() {
-	num1 := 100
-	fmt.Printf("num1的類型 = %T, num1的值 = %v, num1的位置 = %v\n", num1, num1, &num1)
-	num2 := new(int)
-	fmt.Printf("num2的類型 = %T, num2的值 = %v, num2的位置 = %v, num2 指向的值 = %v\n",
-		num2, num2, &num2, *num2)
+	test()
+	fmt.Println("hihi")
 }
