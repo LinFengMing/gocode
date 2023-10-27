f862d40cb7a2d08754243145024a7f383f36f8b4
jiro <jiro853141@gmail.com>
2023-10-27 21:17:15 +0800

    143

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 )
 
 func main() {
-	hen1 := 3.0
-	hen2 := 5.0
-	hen3 := 1.0
-	hen4 := 3.4
-	hen5 := 2.0
-	hen6 := 50.0
-	totalWeight := hen1 + hen2 + hen3 + hen4 + hen5 + hen6
-	avgWeight := fmt.Sprintf("%.2f", totalWeight/6)
+	var hens [7]float64
+	hens[0] = 3.0
+	hens[1] = 5.0
+	hens[2] = 1.0
+	hens[3] = 3.4
+	hens[4] = 2.0
+	hens[5] = 50.0
+	hens[6] = 150.0
+	totalWeight := 0.0
+	for i := 0; i < len(hens); i++ {
+		totalWeight += hens[i]
+	}
+	avgWeight := fmt.Sprintf("%.2f", totalWeight/float64(len(hens)))
 	fmt.Printf("totalWeight = %v, avgWeight = %v", totalWeight, avgWeight)
 }
