c31e8354b1999007c0d477f3f3aa29a6348c42b4
jiro <jiro853141@gmail.com>
2023-10-27 21:03:44 +0800

    142

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func readConf(name string) (err error) {
-	if name == "config.ini" {
-		return nil
-	} else {
-		return errors.New("讀取文件錯誤")
-	}
-}
-
-func test() {
-	err := readConf("config.ini")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("test")
-}
-
 func main() {
-	test()
-	fmt.Println("main")
+	hen1 := 3.0
+	hen2 := 5.0
+	hen3 := 1.0
+	hen4 := 3.4
+	hen5 := 2.0
+	hen6 := 50.0
+	totalWeight := hen1 + hen2 + hen3 + hen4 + hen5 + hen6
+	avgWeight := fmt.Sprintf("%.2f", totalWeight/6)
+	fmt.Printf("totalWeight = %v, avgWeight = %v", totalWeight, avgWeight)
 }
