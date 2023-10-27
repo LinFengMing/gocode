c1a49c1b1bb9d7b2b2d79eff18a49989878a2dfb
jiro <jiro853141@gmail.com>
2023-10-27 23:45:23 +0800

    145

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 )
 
 func main() {
-	var intArr [3]int // 預設值為 0, int64 間隔 8 個字元
-	fmt.Println(intArr)
-	intArr[0] = 10
-	intArr[1] = 20
-	intArr[2] = 30
-	fmt.Println(intArr)
-	fmt.Printf("intArr 的位置 = %p, intArr[0] 的位置 = %p, intArr[1] 的位置 = %p, intArr[2] 的位置 = %p\n",
-		&intArr, &intArr[0], &intArr[1], &intArr[2])
+	// var score [5]float64
+	// for i := 0; i < len(score); i++ {
+	// 	fmt.Printf("請輸入第 %d 個元素的值", i+1)
+	// 	fmt.Scanln(&score[i])
+	// }
+	// for i := 0; i < len(score); i++ {
+	// 	fmt.Printf("score[%d] = %v\n", i, score[i])
+	// }
+	// 初始化陣列的方式
+	var numArr01 [3]int = [3]int{1, 2, 3}
+	fmt.Println("numArr01 =", numArr01)
+	var numArr02 = [3]int{4, 5, 6}
+	fmt.Println("numArr02 =", numArr02)
+	var numArr03 = [...]int{7, 8, 9}
+	fmt.Println("numArr03 =", numArr03)
+	var numArr04 = [...]int{1: 800, 0: 900, 2: 1000}
+	fmt.Println("numArr04 =", numArr04)
+	numArr05 := [...]int{1: 1100, 0: 1300, 2: 1200}
+	fmt.Println("numArr05 =", numArr05)
 }
