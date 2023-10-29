0efb158f47fd1d6ebc60d7501ab9b9e9aa19b49d
jiro <jiro853141@gmail.com>
2023-10-29 22:47:58 +0800

    147

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 )
 
 func main() {
-	var heroes [3]string = [3]string{"卡特蓮娜", "雷尼克頓", "伊澤瑞爾"}
-	for i, v := range heroes {
-		fmt.Printf("i = %v, v = %v\n", i, v)
-	}
-	for _, v := range heroes {
-		fmt.Printf("v = %v\n", v)
-	}
+	var arr01 [3]int
+	arr01[0] = 1
+	arr01[1] = 3
+	arr01[2] = 5
+	fmt.Println(arr01)
+	// 預設值，數字為 0，字串為 ""，boolen 為 falst
+	var arr02 [3]float32
+	var arr03 [3]string
+	var arr04 [3]bool
+	fmt.Printf("arr02 = %v, arr03 = %v, arr04 = %v\n", arr02, arr03, arr04)
 }
