82a4ec97c6333e4b04b9fe0d06cec411b44fba69
jiro <jiro853141@gmail.com>
2023-10-30 22:30:02 +0800

    150

--- main.go
+++ main.go
@@ -5,22 +5,10 @@ import (
 )
 
 func main() {
-	var myChars [26]byte
-	for i := 0; i < 26; i++ {
-		myChars[i] = 'A' + byte(i)
+	var intArr [5]int = [...]int{1, -1, 9, 90, 12}
+	sum := 0
+	for _, v := range intArr {
+		sum += v
 	}
-	for i := 0; i < 26; i++ {
-		fmt.Printf("%c\n", myChars[i])
-	}
-
-	var intArr [5]int = [...]int{1, -1, 9, 90, 11}
-	maxVal := intArr[0]
-	maxIndex := 0
-	for i := 1; i < len(intArr); i++ {
-		if maxVal < intArr[i] {
-			maxVal = intArr[i]
-			maxIndex = i
-		}
-	}
-	fmt.Printf("maxVal = %v, maxIndex = %v", maxVal, maxIndex)
+	fmt.Printf("sum = %v, 平均值 = %v", sum, float64(sum)/float64(len(intArr)))
 }
