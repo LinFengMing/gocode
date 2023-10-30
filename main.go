c24b2c500e5cafd6d168a50eb98fef65bde9be8a
jiro <jiro853141@gmail.com>
2023-10-30 23:13:20 +0800

    151

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	var intArr [5]int = [...]int{1, -1, 9, 90, 12}
-	sum := 0
-	for _, v := range intArr {
-		sum += v
+	var intArr [5]int
+	len := len(intArr)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < len; i++ {
+		intArr[i] = r.Intn(100)
 	}
-	fmt.Printf("sum = %v, 平均值 = %v", sum, float64(sum)/float64(len(intArr)))
+	fmt.Println("交換前", intArr)
+	temp := 0
+	for i := 0; i < len/2; i++ {
+		temp = intArr[len-1-i]
+		intArr[len-1-i] = intArr[i]
+		intArr[i] = temp
+	}
+	fmt.Println("交換後", intArr)
 }
