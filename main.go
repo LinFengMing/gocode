fe56274045dee7192960495623f6d71df909172e
jiro <jiro853141@gmail.com>
2023-11-02 00:48:26 +0800

    152

--- main.go
+++ main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	var intArr [5]int
-	len := len(intArr)
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < len; i++ {
-		intArr[i] = r.Intn(100)
-	}
-	fmt.Println("交換前", intArr)
-	temp := 0
-	for i := 0; i < len/2; i++ {
-		temp = intArr[len-1-i]
-		intArr[len-1-i] = intArr[i]
-		intArr[i] = temp
-	}
-	fmt.Println("交換後", intArr)
+	var intArr [5]int = [...]int{1, 22, 33, 66, 99}
+	// 使用 intArr[1:3] index 1 到 3 不包含 3
+	slice := intArr[1:3]
+	fmt.Println("intArr =", intArr)
+	fmt.Println("slice =", slice)
+	fmt.Println("slice 的元素數 =", len(slice))
+	fmt.Println("slice 的容量 =", cap(slice))
 }
