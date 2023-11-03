5f1731abd238015a55519a02959d8b138fe0cbf4
jiro <jiro853141@gmail.com>
2023-11-03 22:15:16 +0800

    156

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import (
 )
 
 func main() {
-	var slice []float64 = make([]float64, 5, 10)
-	slice[1] = 10
-	slice[3] = 20
-	fmt.Println("slice =", slice)
-	fmt.Println("slice size =", len(slice))
-	fmt.Println("slice cap =", cap(slice))
-	var slice2 []string = []string{"Tom", "Jack", "Marry"}
-	fmt.Println("slice2 =", slice2)
-	fmt.Println("slice2 size =", len(slice2))
-	fmt.Println("slice2 cap =", cap(slice2))
+	var arr [5]int = [...]int{10, 20, 30, 40, 50}
+	slice := arr[1:4]
+	for i := 0; i < len(slice); i++ {
+		fmt.Printf("slice[%v] = %v\n", i, slice[i])
+	}
+	for i, v := range slice {
+		fmt.Printf("i = %v, v = %v\n", i, v)
+	}
 }
