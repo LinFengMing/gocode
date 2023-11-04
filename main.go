55cfe4356586f13b9eb01acf71390882c42658a8
jiro <jiro853141@gmail.com>
2023-11-04 23:19:57 +0800

    157

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 
 func main() {
 	var arr [5]int = [...]int{10, 20, 30, 40, 50}
-	slice := arr[1:4]
-	for i := 0; i < len(slice); i++ {
-		fmt.Printf("slice[%v] = %v\n", i, slice[i])
-	}
+	slice := arr[:] // arr[0:len(arr)]
 	for i, v := range slice {
 		fmt.Printf("i = %v, v = %v\n", i, v)
 	}
+	slice2 := slice[1:2]
+	fmt.Println("slice2", slice2)
 }
