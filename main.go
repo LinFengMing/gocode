f06d291bc88aaa45abc3c24fd798acc717e24bdb
jiro <jiro853141@gmail.com>
2023-11-05 23:35:39 +0800

    160

--- main.go
+++ main.go
@@ -4,26 +4,17 @@ import (
 	"fmt"
 )
 
-func test(slice []int) {
-	slice[0] = 100
-}
-
 func main() {
-	var arr []int = []int{1, 2, 3, 4, 5}
-	slice := make([]int, 1)
-	fmt.Println(slice)
-	copy(slice, arr)
-	fmt.Println(slice)
-	var slice2 []int
-	var arr2 [5]int = [...]int{1, 2, 3, 4, 5}
-	slice2 = arr2[:]
-	var slice3 = slice2
-	slice3[0] = 10
-	fmt.Println("slice3", slice3)
-	fmt.Println("slice2", slice2)
-	fmt.Println("arr2", arr2)
-	slice4 := []int{1, 2, 3, 4}
-	fmt.Println("slice4", slice4)
-	test(slice4)
-	fmt.Println("slice4", slice4)
+	str := "hello@world"
+	slice := str[6:]
+	fmt.Println("slice =", slice)
+	arr := []byte(str)
+	arr[0] = 'H'
+	str = string(arr)
+	fmt.Println("str =", str)
+	// []rune 是按字元處理，可以使用中文
+	arr1 := []rune(str)
+	arr1[0] = '哈'
+	str = string(arr1)
+	fmt.Println("str =", str)
 }
