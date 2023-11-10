f7423c199833ffac4a2c391ff89e3fc225d44a29
jiro <jiro853141@gmail.com>
2023-11-10 21:21:24 +0800

    167

--- main.go
+++ main.go
@@ -4,29 +4,24 @@ import (
 	"fmt"
 )
 
-func main() {
-	names := [4]string{"白眉鷹王", "金毛獅王", "紫衫龍王", "青翼蝠王"}
-	var heroName = ""
-	fmt.Println("請輸入要查詢的人名：")
-	fmt.Scanln(&heroName)
-	for i := range names {
-		if heroName == names[i] {
-			fmt.Printf("找到了，這個人是 %v，編號 %v\n", heroName, i)
-			break
-		} else if i == len(names)-1 {
-			fmt.Println("沒找到")
-		}
+func BinarySearch(arr *[6]int, leftIndex int, rightIndex int, findVal int) {
+	if leftIndex > rightIndex {
+		fmt.Println("找不到")
+		return
 	}
-	index := -1
-	for i := range names {
-		if heroName == names[i] {
-			index = i
-			break
-		}
-	}
-	if index != -1 {
-		fmt.Printf("找到了，這個人是 %v，編號 %v\n", heroName, index)
+
+	middleIndex := (leftIndex + rightIndex) / 2
+
+	if (*arr)[middleIndex] > findVal {
+		BinarySearch(arr, leftIndex, middleIndex-1, findVal)
+	} else if (*arr)[middleIndex] < findVal {
+		BinarySearch(arr, middleIndex+1, rightIndex, findVal)
 	} else {
-		fmt.Println("沒找到")
+		fmt.Printf("找到了，index = %v\n", middleIndex)
 	}
+}
+
+func main() {
+	arr := [6]int{1, 8, 10, 89, 1000, 1234}
+	BinarySearch(&arr, 0, len(arr)-1, 1000)
 }
