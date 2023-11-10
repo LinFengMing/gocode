c07d5d0bf0b8045ac635ff69e6d785548f6d7284
jiro <jiro853141@gmail.com>
2023-11-10 22:41:45 +0800

    168

--- main.go
+++ main.go
@@ -4,24 +4,15 @@ import (
 	"fmt"
 )
 
-func BinarySearch(arr *[6]int, leftIndex int, rightIndex int, findVal int) {
-	if leftIndex > rightIndex {
-		fmt.Println("找不到")
-		return
-	}
-
-	middleIndex := (leftIndex + rightIndex) / 2
-
-	if (*arr)[middleIndex] > findVal {
-		BinarySearch(arr, leftIndex, middleIndex-1, findVal)
-	} else if (*arr)[middleIndex] < findVal {
-		BinarySearch(arr, middleIndex+1, rightIndex, findVal)
-	} else {
-		fmt.Printf("找到了，index = %v\n", middleIndex)
-	}
-}
-
 func main() {
-	arr := [6]int{1, 8, 10, 89, 1000, 1234}
-	BinarySearch(&arr, 0, len(arr)-1, 1000)
+	var arr [4][6]int
+	arr[1][2] = 1
+	arr[2][1] = 2
+	arr[2][3] = 3
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 6; j++ {
+			fmt.Print(arr[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
