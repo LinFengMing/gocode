8b262a4037a1f3922594086fa250cdea1915d4e4
jiro <jiro853141@gmail.com>
2023-11-11 15:31:44 +0800

    169

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 )
 
 func main() {
-	var arr [4][6]int
-	arr[1][2] = 1
-	arr[2][1] = 2
-	arr[2][3] = 3
-	for i := 0; i < 4; i++ {
-		for j := 0; j < 6; j++ {
-			fmt.Print(arr[i][j], " ")
-		}
-		fmt.Println()
-	}
+	var arr [2][3]int
+	arr[1][1] = 10
+	fmt.Println(arr)
+	fmt.Printf("arr[0] 的地址 %p\n", &arr[0])
+	fmt.Printf("arr[1] 的地址 %p\n", &arr[1])
+	fmt.Printf("arr[0][0] 的地址 %p\n", &arr[0][0])
+	fmt.Printf("arr[1][0] 的地址 %p\n", &arr[1][0])
+	var arr2 [2][3]int = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println("arr2 =", arr2)
 }
