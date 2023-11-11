11265fa5b9dc9f11c9db238c8a8ddf73f1db07cb
jiro <jiro853141@gmail.com>
2023-11-11 22:25:03 +0800

    171

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 )
 
 func main() {
-	var arr = [2][3]int{{1, 2, 3}, {4, 5, 6}}
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr[i]); j++ {
-			fmt.Printf("%v\t", arr[i][j])
-		}
-		fmt.Println()
-	}
-	for i, v := range arr {
-		for j, v2 := range v {
-			fmt.Printf("arr[%v][%v] = %v\t", i, j, v2)
+	var scores [3][5]float64
+	for i := 0; i < len(scores); i++ {
+		for j := 0; j < len(scores[i]); j++ {
+			fmt.Printf("請輸入第 %d 個班第 %d 個學生的成績：", i+1, j+1)
+			fmt.Scanln(&scores[i][j])
 		}
 	}
+	total := 0.0
+	sduentNum := 0
+	for i := 0; i < len(scores); i++ {
+		sum := 0.0
+		for j := 0; j < len(scores[i]); j++ {
+			sduentNum++
+			sum += scores[i][j]
+		}
+		total += sum
+		fmt.Printf("第 %d 個班的總分為 %v, 平均分為 %v\n", i+1, sum,
+			sum/float64(len(scores[i])))
+	}
+	fmt.Printf("所有班的總分為 %v, 平均分為 %v\n", total, total/float64(sduentNum))
 }
