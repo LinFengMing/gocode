5bdd37325deec523438a600bacfc1d0fcd903ebe
jiro <jiro853141@gmail.com>
2023-11-13 00:58:51 +0800

    173

--- main.go
+++ main.go
@@ -5,24 +5,10 @@ import (
 )
 
 func main() {
-	var scores [3][5]float64
-	for i := 0; i < len(scores); i++ {
-		for j := 0; j < len(scores[i]); j++ {
-			fmt.Printf("請輸入第 %d 個班第 %d 個學生的成績：", i+1, j+1)
-			fmt.Scanln(&scores[i][j])
-		}
-	}
-	total := 0.0
-	sduentNum := 0
-	for i := 0; i < len(scores); i++ {
-		sum := 0.0
-		for j := 0; j < len(scores[i]); j++ {
-			sduentNum++
-			sum += scores[i][j]
-		}
-		total += sum
-		fmt.Printf("第 %d 個班的總分為 %v, 平均分為 %v\n", i+1, sum,
-			sum/float64(len(scores[i])))
-	}
-	fmt.Printf("所有班的總分為 %v, 平均分為 %v\n", total, total/float64(sduentNum))
+	// map 定義不會分配記憶體空間，必須使用 make 來分配記憶體空間
+	var m map[string]string
+	m = make(map[string]string, 10)
+	m["no1"] = "jiro"
+	m["no2"] = "teru"
+	fmt.Println(m)
 }
