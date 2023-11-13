c7532e5fd303f7a85709df9ed17342affb80dd7c
jiro <jiro853141@gmail.com>
2023-11-14 01:43:49 +0800

    174

--- main.go
+++ main.go
@@ -5,10 +5,23 @@ import (
 )
 
 func main() {
-	// map 定義不會分配記憶體空間，必須使用 make 來分配記憶體空間
-	var m map[string]string
-	m = make(map[string]string, 10)
-	m["no1"] = "jiro"
-	m["no2"] = "teru"
-	fmt.Println(m)
+	cities := make(map[string]string)
+	cities["no1"] = "tokyo"
+	cities["no2"] = "osaka"
+	cities["no3"] = "nagoya"
+	fmt.Println(cities)
+	heroes := map[string]string{
+		"no1": "superman",
+		"no2": "batman",
+		"no3": "spiderman",
+	}
+	fmt.Println(heroes)
+	students := make(map[string]map[string]string)
+	students["no1"] = make(map[string]string, 2)
+	students["no1"]["name"] = "tom"
+	students["no1"]["sex"] = "male"
+	students["no2"] = make(map[string]string, 2)
+	students["no2"]["name"] = "mary"
+	students["no1"]["sex"] = "woman"
+	fmt.Println(students)
 }
