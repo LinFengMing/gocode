ed9d62e96a6e1430ab785af687a45201f70115cb
jiro <jiro853141@gmail.com>
2023-11-17 01:30:22 +0800

    175

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ func main() {
 	cities["no1"] = "tokyo"
 	cities["no2"] = "osaka"
 	cities["no3"] = "nagoya"
+	// key 不存在，就是新增，存在就是修改
+	cities["no3"] = "fukuoka"
+	// delete 指定的 key 不存在時，删除不會執行，也不會報錯
+	delete(cities, "no4")
 	fmt.Println(cities)
-	heroes := map[string]string{
-		"no1": "superman",
-		"no2": "batman",
-		"no3": "spiderman",
+	// 尋找 map 指定 key 是否存在
+	val, ok := cities["no4"]
+	if ok {
+		fmt.Println("no4 key =", val)
+	} else {
+		fmt.Println("no4 key 不存在")
 	}
-	fmt.Println(heroes)
-	students := make(map[string]map[string]string)
-	students["no1"] = make(map[string]string, 2)
-	students["no1"]["name"] = "tom"
-	students["no1"]["sex"] = "male"
-	students["no2"] = make(map[string]string, 2)
-	students["no2"]["name"] = "mary"
-	students["no1"]["sex"] = "woman"
-	fmt.Println(students)
+	// 刪除 map 全部 key
+	cities = make(map[string]string)
+	fmt.Println(cities)
 }
