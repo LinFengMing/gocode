9811ef12f54bc7962d104caf78eda67a7542e0f4
jiro <jiro853141@gmail.com>
2023-11-19 22:53:13 +0800

    178

--- main.go
+++ main.go
@@ -2,27 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	// slice of map
-	var monsters []map[string]string
-	monsters = make([]map[string]string, 2)
-	if monsters[0] == nil {
-		monsters[0] = make(map[string]string, 2)
-		monsters[0]["name"] = "牛魔王"
-		monsters[0]["age"] = "500"
+	map1 := make(map[int]int)
+	map1[10] = 100
+	map1[1] = 13
+	map1[4] = 56
+	map1[8] = 90
+	fmt.Println(map1)
+	// map 的 key 放入 slice
+	var keys []int
+	for k, _ := range map1 {
+		keys = append(keys, k)
 	}
-	if monsters[1] == nil {
-		monsters[1] = make(map[string]string, 2)
-		monsters[1]["name"] = "玉兔精"
-		monsters[1]["age"] = "400"
+	// slice 排序
+	sort.Ints(keys)
+	fmt.Println(keys)
+	for _, k := range keys {
+		fmt.Printf("map1[%v] = %v\n", k, map1[k])
 	}
-	// slice append
-	newMonster := map[string]string{
-		"name": "火雲邪神",
-		"age":  "200",
-	}
-	monsters = append(monsters, newMonster)
-	fmt.Println(monsters)
 }
