b9011f222db56594d0cf6ba3b59fecd2a20cc225
jiro <jiro853141@gmail.com>
2023-11-21 23:38:32 +0800

    179

--- main.go
+++ main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
+
+func modify(map1 map[int]int) {
+	map1[10] = 900
+}
+
+type Stu struct {
+	Name string
+	Age  int
+}
 
 func main() {
 	map1 := make(map[int]int)
-	map1[10] = 100
-	map1[1] = 13
-	map1[4] = 56
-	map1[8] = 90
+	map1[1] = 90
+	map1[2] = 80
+	map1[10] = 1
+	map1[20] = 2
+	modify(map1)
 	fmt.Println(map1)
-	// map 的 key 放入 slice
-	var keys []int
-	for k, _ := range map1 {
-		keys = append(keys, k)
-	}
-	// slice 排序
-	sort.Ints(keys)
-	fmt.Println(keys)
-	for _, k := range keys {
-		fmt.Printf("map1[%v] = %v\n", k, map1[k])
+	// map value use struct
+	students := make(map[string]Stu, 10)
+	stu1 := Stu{"tom", 18}
+	stu2 := Stu{"mary", 19}
+	students["no1"] = stu1
+	students["no2"] = stu2
+	fmt.Println(students)
+	for k, v := range students {
+		fmt.Printf("學生的編號是 %v\n", k)
+		fmt.Printf("學生的名字是 %v\n", v.Name)
+		fmt.Printf("學生的年齡是 %v\n", v.Age)
+		fmt.Println()
 	}
 }
