e5b259695f82a9609b974c6ee286c7981ac4c362
jiro <jiro853141@gmail.com>
2023-11-22 01:56:46 +0800

    180

--- main.go
+++ main.go
@@ -4,34 +4,29 @@ import (
 	"fmt"
 )
 
-func modify(map1 map[int]int) {
-	map1[10] = 900
-}
-
 type Stu struct {
 	Name string
 	Age  int
 }
 
-func main() {
-	map1 := make(map[int]int)
-	map1[1] = 90
-	map1[2] = 80
-	map1[10] = 1
-	map1[20] = 2
-	modify(map1)
-	fmt.Println(map1)
-	// map value use struct
-	students := make(map[string]Stu, 10)
-	stu1 := Stu{"tom", 18}
-	stu2 := Stu{"mary", 19}
-	students["no1"] = stu1
-	students["no2"] = stu2
-	fmt.Println(students)
-	for k, v := range students {
-		fmt.Printf("學生的編號是 %v\n", k)
-		fmt.Printf("學生的名字是 %v\n", v.Name)
-		fmt.Printf("學生的年齡是 %v\n", v.Age)
-		fmt.Println()
+func modifyUser(users map[string]map[string]string, name string) {
+	// 判断users中是否有name
+	if users[name] != nil {
+		users[name]["pwd"] = "888888"
+	} else {
+		users[name] = make(map[string]string, 2)
+		users[name]["pwd"] = "888888"
+		users[name]["nickname"] = "暱稱~" + name
 	}
+}
+
+func main() {
+	users := make(map[string]map[string]string, 10)
+	users["smith"] = make(map[string]string, 2)
+	users["smith"]["pwd"] = "999999"
+	users["smith"]["nickname"] = "小花貓"
+	modifyUser(users, "tom")
+	modifyUser(users, "mary")
+	modifyUser(users, "smith")
+	fmt.Println(users)
 }
