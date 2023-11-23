9c9f50ee947c7dc8a2d9846535a01a917cbfd7e3
jiro <jiro853141@gmail.com>
2023-11-24 01:51:13 +0800

    183

--- main.go
+++ main.go
@@ -4,29 +4,23 @@ import (
 	"fmt"
 )
 
-type Stu struct {
-	Name string
-	Age  int
-}
-
-func modifyUser(users map[string]map[string]string, name string) {
-	// 判断users中是否有name
-	if users[name] != nil {
-		users[name]["pwd"] = "888888"
-	} else {
-		users[name] = make(map[string]string, 2)
-		users[name]["pwd"] = "888888"
-		users[name]["nickname"] = "暱稱~" + name
-	}
+type Cat struct {
+	Name  string
+	Age   int
+	Color string
+	Hobby string
 }
 
 func main() {
-	users := make(map[string]map[string]string, 10)
-	users["smith"] = make(map[string]string, 2)
-	users["smith"]["pwd"] = "999999"
-	users["smith"]["nickname"] = "小花貓"
-	modifyUser(users, "tom")
-	modifyUser(users, "mary")
-	modifyUser(users, "smith")
-	fmt.Println(users)
+	var cat1 Cat
+	cat1.Name = "小白"
+	cat1.Age = 3
+	cat1.Color = "白色"
+	cat1.Hobby = "吃魚"
+	fmt.Println("cat1 =", cat1)
+	fmt.Println("貓的資料如下：")
+	fmt.Println("name =", cat1.Name)
+	fmt.Println("age =", cat1.Age)
+	fmt.Println("color =", cat1.Color)
+	fmt.Println("hobby =", cat1.Hobby)
 }
