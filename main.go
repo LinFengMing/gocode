7f7abbcbf42bbad8c4bde0d05724e6720484bdaf
jiro <jiro853141@gmail.com>
2023-11-27 20:26:48 +0800

    186

--- main.go
+++ main.go
@@ -5,42 +5,21 @@ import (
 )
 
 type Person struct {
-	Name   string
-	Age    int
-	Scores [5]float64
-	ptr    *int
-	slice  []int
-	map1   map[string]string
-}
-
-type Monster struct {
 	Name string
 	Age  int
 }
 
 func main() {
 	var p1 Person
+	p1.Name = "John"
+	p1.Age = 25
 	fmt.Println(p1)
-	if p1.ptr == nil {
-		fmt.Println("ok1")
-	}
-	if p1.slice == nil {
-		fmt.Println("ok2")
-	}
-	if p1.map1 == nil {
-		fmt.Println("ok3")
-	}
-	p1.ptr = new(int)
-	p1.slice = make([]int, 10)
-	p1.slice[0] = 100
-	p1.map1 = make(map[string]string)
-	p1.map1["key1"] = "Tom"
-	fmt.Println(p1)
-	var monster1 Monster
-	monster1.Name = "牛魔王"
-	monster1.Age = 500
-	monster2 := monster1
-	monster2.Name = "青牛精"
-	fmt.Println(monster1)
-	fmt.Println(monster2)
+	var p2 = Person{"John", 25}
+	fmt.Println(p2)
+	var p3 *Person = new(Person)
+	(*p3).Name = "John"
+	(*p3).Age = 25
+	fmt.Println(*p3)
+	var p4 *Person = &Person{"John", 25}
+	fmt.Println(*p4)
 }
