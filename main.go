8d007c64c7ff6df4363f4c8984d1157de3e9555e
jiro <jiro853141@gmail.com>
2023-12-03 01:48:27 +0800

    194

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ type Circle struct {
 	radius float64
 }
 
-func (c Circle) area() float64 {
-	return 3.14 * c.radius * c.radius
+func (c *Circle) area() float64 {
+	fmt.Printf("c 指向的位址 = %p\n", c)
+	c.radius = 10.0
+	return 3.14 * (*c).radius * (*c).radius
 }
 
 func main() {
 	var c Circle
-	c.radius = 4.0
-	res := c.area()
+	fmt.Printf("main c 位址 = %p\n", &c)
+	c.radius = 5.0
+	res := (&c).area()
 	fmt.Println("Area of Circle(c) is", res)
+	fmt.Println("c.radius =", c.radius)
 }
