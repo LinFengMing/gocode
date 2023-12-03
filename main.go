f2f007c279ca7c57e5b42e59a5c295485b489541
jiro <jiro853141@gmail.com>
2023-12-03 23:44:19 +0800

    197

--- main.go
+++ main.go
@@ -7,33 +7,72 @@ import (
 type MenthodUtils struct {
 }
 
-func (m MenthodUtils) Print() {
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 8; j++ {
-			fmt.Print("*")
+func (m *MenthodUtils) JudgnNum(num int) {
+	if num%2 == 0 {
+		fmt.Println(num, "是偶數")
+	} else {
+		fmt.Println(num, "是奇數")
+	}
+}
+
+func (mu *MenthodUtils) Print(n int, m int, key string) {
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			fmt.Print(key)
 		}
 		fmt.Println()
 	}
 }
 
-func (m MenthodUtils) Print2(m1 int, m2 int) {
-	for i := 0; i < m1; i++ {
-		for j := 0; j < m2; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println()
-	}
+type Calcuator struct {
+	Num1 float64
+	Num2 float64
 }
 
-func (m MenthodUtils) Area(len float64, width float64) float64 {
-	return len * width
+func (c *Calcuator) GetSum() float64 {
+	return c.Num1 + c.Num2
+}
+
+func (c *Calcuator) GetSub() float64 {
+	return c.Num1 - c.Num2
+}
+
+func (c *Calcuator) GetMul() float64 {
+	return c.Num1 * c.Num2
+}
+
+func (c *Calcuator) GetDiv() float64 {
+	return c.Num1 / c.Num2
+}
+
+func (c *Calcuator) GetRes(operator byte) float64 {
+	res := 0.0
+	switch operator {
+	case '+':
+		res = c.Num1 + c.Num2
+	case '-':
+		res = c.Num1 - c.Num2
+	case '*':
+		res = c.Num1 * c.Num2
+	case '/':
+		res = c.Num1 / c.Num2
+	default:
+		fmt.Println("運算符號錯誤")
+	}
+	return res
 }
 
 func main() {
-	var m = MenthodUtils{}
-	m.Print()
+	var mu MenthodUtils
+	mu.JudgnNum(10)
 	fmt.Println()
-	m.Print2(2, 3)
-	fmt.Println()
-	fmt.Println(m.Area(2.0, 3.0))
+	mu.Print(5, 5, "#")
+	var c Calcuator
+	c.Num1 = 1.2
+	c.Num2 = 2.2
+	fmt.Printf("sum = %v\n", fmt.Sprintf("%.2f", c.GetSum()))
+	fmt.Printf("sub = %v\n", fmt.Sprintf("%.2f", c.GetSub()))
+	fmt.Printf("mul = %v\n", fmt.Sprintf("%.2f", c.GetMul()))
+	fmt.Printf("sub = %v\n", fmt.Sprintf("%.2f", c.GetDiv()))
+	fmt.Printf("res = %v\n", fmt.Sprintf("%.2f", c.GetRes('+')))
 }
