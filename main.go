77f76f7d4dcca768011ba28540854bb24000dfd3
jiro <jiro853141@gmail.com>
2023-12-04 21:59:05 +0800

    198

--- main.go
+++ main.go
@@ -4,75 +4,37 @@ import (
 	"fmt"
 )
 
-type MenthodUtils struct {
+type Person struct {
+	Name string
+	Age  int
 }
 
-func (m *MenthodUtils) JudgnNum(num int) {
-	if num%2 == 0 {
-		fmt.Println(num, "是偶數")
-	} else {
-		fmt.Println(num, "是奇數")
-	}
+func test01(p Person) {
+	fmt.Println(p.Name)
 }
 
-func (mu *MenthodUtils) Print(n int, m int, key string) {
-	for i := 1; i < n; i++ {
-		for j := 1; j < m; j++ {
-			fmt.Print(key)
-		}
-		fmt.Println()
-	}
+func test02(p *Person) {
+	fmt.Println(p.Name)
 }
 
-type Calcuator struct {
-	Num1 float64
-	Num2 float64
+func (p Person) test03() { // 這裡是值拷貝
+	p.Name = "jack"
+	fmt.Println(p.Name) // jack
 }
 
-func (c *Calcuator) GetSum() float64 {
-	return c.Num1 + c.Num2
-}
-
-func (c *Calcuator) GetSub() float64 {
-	return c.Num1 - c.Num2
-}
-
-func (c *Calcuator) GetMul() float64 {
-	return c.Num1 * c.Num2
-}
-
-func (c *Calcuator) GetDiv() float64 {
-	return c.Num1 / c.Num2
-}
-
-func (c *Calcuator) GetRes(operator byte) float64 {
-	res := 0.0
-	switch operator {
-	case '+':
-		res = c.Num1 + c.Num2
-	case '-':
-		res = c.Num1 - c.Num2
-	case '*':
-		res = c.Num1 * c.Num2
-	case '/':
-		res = c.Num1 / c.Num2
-	default:
-		fmt.Println("運算符號錯誤")
-	}
-	return res
+func (p *Person) test04() { // 這裡是位址拷貝
+	p.Name = "mary"
+	fmt.Println(p.Name) // mary
 }
 
 func main() {
-	var mu MenthodUtils
-	mu.JudgnNum(10)
-	fmt.Println()
-	mu.Print(5, 5, "#")
-	var c Calcuator
-	c.Num1 = 1.2
-	c.Num2 = 2.2
-	fmt.Printf("sum = %v\n", fmt.Sprintf("%.2f", c.GetSum()))
-	fmt.Printf("sub = %v\n", fmt.Sprintf("%.2f", c.GetSub()))
-	fmt.Printf("mul = %v\n", fmt.Sprintf("%.2f", c.GetMul()))
-	fmt.Printf("sub = %v\n", fmt.Sprintf("%.2f", c.GetDiv()))
-	fmt.Printf("res = %v\n", fmt.Sprintf("%.2f", c.GetRes('+')))
+	p := Person{"tom", 20}
+	test01(p)
+	test02(&p)
+	p.test03()
+	fmt.Println(p.Name) // tom
+	(&p).test03()       // 這裡值拷貝
+	fmt.Println(p.Name) // tom
+	(&p).test04()       // 這裡是位址拷貝
+	fmt.Println(p.Name) // mary
 }
