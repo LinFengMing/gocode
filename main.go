328b84801079615a0a8b4ce2ab575f224f4d04e8
jiro <jiro853141@gmail.com>
2023-12-06 20:49:51 +0800

    205

--- main.go
+++ main.go
@@ -2,57 +2,13 @@ package main
 
 import (
 	"fmt"
+	"gocode/model"
 )
 
-type Account struct {
-	AccountNo string
-	Pwd       string
-	Balance   float64
-}
-
-func (account *Account) Deposite(money float64, pwd string) {
-	if pwd != account.Pwd {
-		fmt.Println("密碼錯誤")
-		return
-	}
-	if money <= 0 {
-		fmt.Println("金額錯誤")
-		return
-	}
-	account.Balance += money
-	fmt.Println("存款成功")
-}
-
-func (account *Account) WithDraw(money float64, pwd string) {
-	if pwd != account.Pwd {
-		fmt.Println("密碼錯誤")
-		return
-	}
-	if money <= 0 || money > account.Balance {
-		fmt.Println("金額錯誤")
-		return
-	}
-	account.Balance -= money
-	fmt.Println("提款成功")
-}
-
-func (account *Account) Query(pwd string) {
-	if pwd != account.Pwd {
-		fmt.Println("密碼錯誤")
-		return
-	}
-	fmt.Printf("帳號:%v 餘額:%v\n", account.AccountNo, account.Balance)
-}
-
 func main() {
-	account := Account{
-		AccountNo: "gs123456",
-		Pwd:       "666666",
-		Balance:   100.0,
-	}
-	account.Query("666666")
-	account.Deposite(200.0, "666666")
-	account.Query("666666")
-	account.WithDraw(150.0, "666666")
-	account.Query("666666")
+	p := model.NewPerson("smith")
+	p.SetAge(18)
+	p.SetSal(5000)
+	fmt.Println(*p)
+	fmt.Println(p.Name, "age =", p.GetAge(), "sal =", p.GetSal())
 }
