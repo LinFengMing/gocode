0d7452958e76f38e224c0bce3fb8166d385ad8fe
jiro <jiro853141@gmail.com>
2023-12-10 00:36:41 +0800

    210

--- main.go
+++ main.go
@@ -4,53 +4,33 @@ import (
 	"fmt"
 )
 
-type Student struct {
-	Name  string
-	Age   int
-	Score int
+type A struct {
+	Name string
+	age  int
 }
 
-func (s *Student) ShowInfo() {
-	fmt.Printf("Name = %v Age = %v Score = %v\n", s.Name, s.Age, s.Score)
+func (a *A) SayOk() {
+	fmt.Println("A SayOk", a.Name)
 }
 
-func (s *Student) SetScore(score int) {
-	s.Score = score
+func (a *A) hello() {
+	fmt.Println("A hello", a.Name)
 }
 
-func (s *Student) getSum(n1 int, n2 int) int {
-	return n1 + n2
+type B struct {
+	A
+	Name string
 }
 
-type Pupil struct {
-	Student
-}
-
-func (p *Pupil) Testing() {
-	fmt.Println("PrimarySchoolStydent is testing...")
-}
-
-type Graduate struct {
-	Student
-}
-
-func (g *Graduate) Testing() {
-	fmt.Println("Graduate is testing...")
+func (b *B) SayOk() {
+	fmt.Println("B SayOk", b.Name)
 }
 
 func main() {
-	pupil := &Pupil{}
-	pupil.Student.Name = "tom"
-	pupil.Student.Age = 8
-	pupil.Testing()
-	pupil.Student.SetScore(80)
-	pupil.Student.ShowInfo()
-	fmt.Println("res =", pupil.Student.getSum(1, 2))
-	graduate := &Graduate{}
-	graduate.Student.Name = "mary"
-	graduate.Student.Age = 28
-	graduate.Testing()
-	graduate.Student.SetScore(90)
-	graduate.Student.ShowInfo()
-	fmt.Println("res =", graduate.Student.getSum(1, 2))
+	var b B
+	b.Name = "tom"
+	b.A.Name = "jack"
+	b.age = 19
+	b.SayOk()
+	b.hello()
 }
