44737442ee770a94091abb2e474f300cde371907
jiro <jiro853141@gmail.com>
2023-12-10 22:25:00 +0800

    212

--- main.go
+++ main.go
@@ -4,20 +4,13 @@ import (
 	"fmt"
 )
 
-type A struct {
+type Monster struct {
 	Name string
 	Age  int
 }
-type B struct {
-	Name  string
-	Score float64
-}
-type C struct {
-	A
-	B
-}
-type D struct {
-	a A
+type E struct {
+	Monster
+	int
 }
 type Goods struct {
 	Name  string
@@ -31,44 +24,13 @@ type TV struct {
 	Goods
 	Brand
 }
-type TV2 struct {
-	*Goods
-	*Brand
-}
 
 func main() {
-	var c C
-	c.A.Name = "tom"
-	c.B.Name = "jack"
-	fmt.Println(c.A.Name) // tom
-	fmt.Println(c.B.Name) // jack
-	var d D
-	d.a.Name = "john"
-	fmt.Println(d.a.Name) // john
-	tv := TV{Goods{"電視機001", 5000.99}, Brand{"海爾", "山東"}}
-	fmt.Println(tv)
-	tv2 := TV{
-		Goods{
-			Name:  "電視機002",
-			Price: 6000.99,
-		},
-		Brand{
-			Name:    "夏普",
-			Address: "北京",
-		},
-	}
-	fmt.Println(tv2)
-	tv3 := TV2{&Goods{"電視機003", 7000.99}, &Brand{"東芝", "日本"}}
-	fmt.Println(*tv3.Goods, *tv3.Brand)
-	tv4 := TV2{
-		&Goods{
-			Name:  "電視機004",
-			Price: 8000.99,
-		},
-		&Brand{
-			Name:    "索尼",
-			Address: "日本",
-		},
-	}
-	fmt.Println(*tv4.Goods, *tv4.Brand)
+	var e E
+	e.Name = "牛魔王"
+	e.Age = 500
+	e.int = 20
+	fmt.Println(e)
+	tv := TV{Goods{"電視機001", 5000.99}, Brand{"索尼", "日本"}}
+	fmt.Println(tv.Goods.Name)
 }
