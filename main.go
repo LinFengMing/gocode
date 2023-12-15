6319d4c9f4d1deb4f784f90195066f82dd653b97
jiro <jiro853141@gmail.com>
2023-12-15 22:55:58 +0800

    220

--- main.go
+++ main.go
@@ -2,48 +2,35 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
 )
 
-type Hero struct {
+type Monkey struct {
 	Name string
-	Age  int
 }
-type HeroSlice []Hero
+type BirdAble interface {
+	flying()
+}
+type FishAble interface {
+	swimming()
+}
 
-// 實作 Interface 介面
-func (hs HeroSlice) Len() int {
-	return len(hs)
+func (m *Monkey) climbing() {
+	fmt.Println(m.Name, "生來會爬樹")
 }
-func (hs HeroSlice) Less(i, j int) bool {
-	// 按年齡從小到大排序
-	return hs[i].Age < hs[j].Age
-	// 按名稱從大到小排序
-	// return hs[i].Name > hs[j].Name
+
+type LittleMonkey struct {
+	Monkey
 }
-func (hs HeroSlice) Swap(i, j int) {
-	hs[i], hs[j] = hs[j], hs[i]
+
+func (l *LittleMonkey) flying() {
+	fmt.Println(l.Name, "學會飛翔")
+}
+func (l *LittleMonkey) swimming() {
+	fmt.Println(l.Name, "學會游泳")
 }
 func main() {
-	var intSlice = []int{0, -1, 10, 7, 90}
-	sort.Ints(intSlice)
-	fmt.Println(intSlice)
-	var heroes HeroSlice
-	for i := 0; i < 10; i++ {
-		hero := Hero{
-			Name: fmt.Sprintf("英雄-%d", rand.Intn(100)),
-			Age:  rand.Intn(100),
-		}
-		heroes = append(heroes, hero)
-	}
-	for _, v := range heroes {
-		fmt.Println(v)
-	}
-	// 使用 sort.Sort 排序
-	sort.Sort(heroes)
-	fmt.Println("排序後")
-	for _, v := range heroes {
-		fmt.Println(v)
-	}
+	monkey := LittleMonkey{Monkey{"悟空"}}
+	monkey.climbing()
+	monkey.flying()
+	monkey.swimming()
 }
