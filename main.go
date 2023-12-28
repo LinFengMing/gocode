9d47d34d7cf6694b9b59519768c4bd12c3bff80d
jiro <jiro853141@gmail.com>
2023-12-28 17:24:41 +0800

    223

--- main.go
+++ main.go
@@ -4,36 +4,27 @@ import (
 	"fmt"
 )
 
-type Usb interface {
-	Start()
-	Stop()
-}
-type Phone struct {
-	name string
+type Point struct {
+	x int
+	y int
 }
 
-func (p Phone) Start() {
-	fmt.Println("手機開始工作")
-}
-func (p Phone) Stop() {
-	fmt.Println("手機停止工作")
-}
-
-type Camera struct {
-	name string
-}
-
-func (c Camera) Start() {
-	fmt.Println("相機開始工作")
-}
-func (c Camera) Stop() {
-	fmt.Println("相機停止工作")
-}
 func main() {
-	// 定義多態數組
-	var usbArr [3]Usb
-	usbArr[0] = Phone{"小米手機"}
-	usbArr[1] = Phone{"華為手機"}
-	usbArr[2] = Camera{"索尼相機"}
-	fmt.Println(usbArr)
+	var a interface{}
+	var point Point = Point{1, 2}
+	a = point
+	var b Point
+	// b = a // error
+	b = a.(Point) // 型別斷言
+	fmt.Println(b)
+	var x interface{}
+	var b2 float32 = 1.1
+	x = b2
+	if y, flag := x.(float32); flag {
+		fmt.Println("轉換成功")
+		fmt.Printf("y 的型別是 %T 值是 = %v\n", y, y)
+	} else {
+		fmt.Println("轉換失敗")
+	}
+	fmt.Println("繼續執行")
 }
