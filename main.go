4412b1a2840c5144c510f883f0b62d65c0b8ff04
jiro <jiro853141@gmail.com>
2023-12-28 20:38:54 +0800

    224

--- main.go
+++ main.go
@@ -4,27 +4,55 @@ import (
 	"fmt"
 )
 
-type Point struct {
-	x int
-	y int
+type Usb interface {
+	Start()
+	Stop()
+}
+type Phone struct {
+	name string
 }
 
-func main() {
-	var a interface{}
-	var point Point = Point{1, 2}
-	a = point
-	var b Point
-	// b = a // error
-	b = a.(Point) // 型別斷言
-	fmt.Println(b)
-	var x interface{}
-	var b2 float32 = 1.1
-	x = b2
-	if y, flag := x.(float32); flag {
-		fmt.Println("轉換成功")
-		fmt.Printf("y 的型別是 %T 值是 = %v\n", y, y)
-	} else {
-		fmt.Println("轉換失敗")
+func (p Phone) Start() {
+	fmt.Println("手機開始工作")
+}
+func (p Phone) Stop() {
+	fmt.Println("手機停止工作")
+}
+func (p Phone) Call() {
+	fmt.Println("手機打電話")
+}
+
+type Camera struct {
+	name string
+}
+
+func (c Camera) Start() {
+	fmt.Println("相機開始工作")
+}
+func (c Camera) Stop() {
+	fmt.Println("相機停止工作")
+}
+
+type Computer struct {
+}
+
+func (c Computer) Working(usb Usb) {
+	usb.Start()
+	// 如果 usb 指向的是 Phone 結構體變量，則調用 Call 方法
+	if phone, flag := usb.(Phone); flag {
+		phone.Call()
 	}
-	fmt.Println("繼續執行")
+	usb.Stop()
+}
+func main() {
+	// 定義多態數組
+	var usbArr [3]Usb
+	usbArr[0] = Phone{"小米手機"}
+	usbArr[1] = Phone{"華為手機"}
+	usbArr[2] = Camera{"索尼相機"}
+	var computer Computer
+	for _, value := range usbArr {
+		computer.Working(value)
+		fmt.Println()
+	}
 }
