cbc94eaad828d2642c4e9b8692bb6f61d9fd1062
jiro <jiro853141@gmail.com>
2024-01-06 23:24:02 +0800

    245

--- main.go
+++ main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+	// read file
+	data, err := os.ReadFile("test.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer file.Close()
-	reader := bufio.NewReader(file)
-	for {
-		str, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		fmt.Print(str)
-	}
-	fmt.Println("End of reding file")
+	fmt.Printf("%v", string(data))
 }
