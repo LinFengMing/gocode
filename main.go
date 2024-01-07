e8b8be27c14280a0ba5ac7705909da040c8ae2a9
jiro <jiro853141@gmail.com>
2024-01-07 23:18:10 +0800

    248

--- main.go
+++ main.go
@@ -1,56 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
-	// 覆蓋寫入
-	file, err := os.OpenFile("abc.txt", os.O_WRONLY|os.O_TRUNC, 0666)
+	filepath := "abc.txt"
+	filepath2 := "kkk.txt"
+	data, err := os.ReadFile(filepath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer file.Close()
-	str := "您好, 林先生!\r\n"
-	writer := bufio.NewWriter(file)
-	for i := 0; i < 10; i++ {
-		writer.WriteString(str)
-	}
-	writer.Flush()
-	// 追加寫入
-	file, err = os.OpenFile("abc.txt", os.O_WRONLY|os.O_APPEND, 0666)
+	err = os.WriteFile(filepath2, data, 0666)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	str = "ABC, ENGLISG!\r\n"
-	writer = bufio.NewWriter(file)
-	for i := 0; i < 10; i++ {
-		writer.WriteString(str)
-	}
-	writer.Flush()
-	// 讀取後輸出，追加寫入
-	file, err = os.OpenFile("abc.txt", os.O_RDWR|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	reader := bufio.NewReader(file)
-	for {
-		str, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		fmt.Print(str)
-	}
-	str = "hello, 台北!\r\n"
-	writer = bufio.NewWriter(file)
-	for i := 0; i < 5; i++ {
-		writer.WriteString(str)
-	}
-	writer.Flush()
 }
