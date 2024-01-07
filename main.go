294f25c5468de45467f58885a464e56ff003ba2f
jiro <jiro853141@gmail.com>
2024-01-08 01:15:35 +0800

    251

--- main.go
+++ main.go
@@ -1,48 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
-type CharCount struct {
-	ChCount    int
-	NumCount   int
-	SpaceCount int
-	OtherCount int
-}
-
 func main() {
-	file, err := os.Open("abc.txt")
-	if err != nil {
-		fmt.Printf("open file err = %v\n", err)
-		return
+	fmt.Println("命令行参数有", len(os.Args))
+	for i, v := range os.Args {
+		fmt.Printf("args[%v] = %v\n", i, v)
 	}
-	defer file.Close()
-	var count CharCount
-	reader := bufio.NewReader(file)
-	for {
-		str, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		for _, v := range str {
-			switch {
-			case v >= 'a' && v <= 'z':
-				fallthrough
-			case v >= 'A' && v <= 'Z':
-				count.ChCount++
-			case v == ' ' || v == '\t':
-				count.SpaceCount++
-			case v >= '0' && v <= '9':
-				count.NumCount++
-			default:
-				count.OtherCount++
-			}
-		}
-	}
-	fmt.Printf("char count = %v, num count = %v, space count = %v, other count = %v\n",
-		count.ChCount, count.NumCount, count.SpaceCount, count.OtherCount)
 }
