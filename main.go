98bfa5c67397b203e7a2ba9b5c64055a6e84c7cf
jiro <jiro853141@gmail.com>
2024-01-07 23:43:49 +0800

    249

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
-	filepath := "abc.txt"
-	filepath2 := "kkk.txt"
-	data, err := os.ReadFile(filepath)
+func CopyFile(dstFileName, srcFileName string) (written int64, err error) {
+	srcFile, err := os.Open(srcFileName)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("open file err = %v\n", err)
 		return
 	}
-	err = os.WriteFile(filepath2, data, 0666)
+	defer srcFile.Close()
+	reader := bufio.NewReader(srcFile)
+	desFile, err := os.OpenFile(dstFileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("open file err = %v\n", err)
 		return
+	}
+	writer := bufio.NewWriter(desFile)
+	defer desFile.Close()
+	return io.Copy(writer, reader)
+}
+func main() {
+	srcFile := "gril.jpg"
+	dstFile := "gril2.jpg"
+	_, err := CopyFile(dstFile, srcFile)
+	if err == nil {
+		fmt.Println("拷貝完成")
+	} else {
+		fmt.Printf("拷貝錯誤 err = %v\n", err)
 	}
 }
