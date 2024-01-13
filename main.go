168a5ca81d842403f7f08ec903fb6f134c366073
jiro <jiro853141@gmail.com>
2024-01-14 02:28:06 +0800

    269

--- main.go
+++ main.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 var (
-	myMap = make(map[int]int, 10)
+	myMap = make(map[int64]int64, 10)
+	// lock 是一個全域互斥鎖
+	lock sync.Mutex
 )
 
 func test(n int) {
-	res := 1
+	res := int64(1)
 	for i := 1; i <= n; i++ {
-		res *= i
+		res *= int64(i)
 	}
-	myMap[n] = res // 併發寫入，fatal error: concurrent map writes
+	lock.Lock()
+	myMap[int64(n)] = res
+	lock.Unlock()
 }
 
 func main() {
-	for i := 1; i <= 200; i++ {
+	for i := 1; i <= 20; i++ {
 		go test(i)
 	}
+	time.Sleep(time.Second * 5)
+	lock.Lock()
 	for i, v := range myMap {
 		fmt.Printf("map[%d] = %d\n", i, v)
 	}
+	lock.Unlock()
 }
