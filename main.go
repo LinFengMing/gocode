d69c00f03a3567aa8d4cf8d4eef86d92d9a2530d
jiro <jiro853141@gmail.com>
2024-01-13 12:40:47 +0800

    268

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 )
 
+var (
+	myMap = make(map[int]int, 10)
+)
+
+func test(n int) {
+	res := 1
+	for i := 1; i <= n; i++ {
+		res *= i
+	}
+	myMap[n] = res // 併發寫入，fatal error: concurrent map writes
+}
+
 func main() {
-	cpuNum := runtime.NumCPU()
-	fmt.Println("cpuNum =", cpuNum)
-	// go1.8 前設定 go 程式執行的最大的 cpu 核數
-	runtime.GOMAXPROCS(cpuNum - 1)
-	fmt.Println("ok")
+	for i := 1; i <= 200; i++ {
+		go test(i)
+	}
+	for i, v := range myMap {
+		fmt.Printf("map[%d] = %d\n", i, v)
+	}
 }
