b3284486e076a9b199eb7118b30951c258ecce14
jiro <jiro853141@gmail.com>
2024-01-14 21:39:39 +0800

    271

--- main.go
+++ main.go
@@ -1,35 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-var (
-	myMap = make(map[int64]int64, 10)
-	// lock 是一個全域互斥鎖
-	lock sync.Mutex
-)
-
-func test(n int) {
-	res := int64(1)
-	for i := 1; i <= n; i++ {
-		res *= int64(i)
-	}
-	lock.Lock()
-	myMap[int64(n)] = res
-	lock.Unlock()
-}
+import "fmt"
 
 func main() {
-	for i := 1; i <= 20; i++ {
-		go test(i)
-	}
-	time.Sleep(time.Second * 5)
-	lock.Lock()
-	for i, v := range myMap {
-		fmt.Printf("map[%d] = %d\n", i, v)
-	}
-	lock.Unlock()
+	var intChan chan int
+	intChan = make(chan int, 3)
+	fmt.Printf("intChan 的值 = %v, intChan本身的位址 = %p\n", intChan, &intChan)
+	intChan <- 10
+	num := 211
+	intChan <- num
+	intChan <- 50
+	// intChan <- 666 // deadlock
+	fmt.Printf("intChan len = %v, cap = %v\n", len(intChan), cap(intChan)) // 3, 3
+	var num2 int
+	num2 = <-intChan
+	fmt.Println("num2 = ", num2)
+	fmt.Printf("intChan len = %v, cap = %v\n", len(intChan), cap(intChan)) // 2, 3
+	num3 := <-intChan
+	num4 := <-intChan
+	fmt.Println("num3 = ", num3)
+	fmt.Println("num4 = ", num4)
+	fmt.Printf("intChan len = %v, cap = %v\n", len(intChan), cap(intChan)) // 0, 3
+	// num5 := <-intChan // deadlock
 }
