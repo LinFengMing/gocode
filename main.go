e0f8ec84dd51cf86e98e03d97228cb0f3d27fd6e
jiro <jiro853141@gmail.com>
2024-01-17 23:52:17 +0800

    279

--- main.go
+++ main.go
@@ -2,57 +2,38 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func putNum(intChan chan int) {
-	for i := 1; i <= 8000; i++ {
-		intChan <- i
+func send(ch chan<- int, exitChan chan struct{}) {
+	for i := 0; i < 10; i++ {
+		ch <- i
 	}
-	close(intChan)
+	close(ch)
+	var a struct{}
+	exitChan <- a
 }
-func primeNum(intChan chan int, primeChan chan int, exitChan chan bool) {
-	var flag bool
+func recv(ch <-chan int, exitChan chan struct{}) {
 	for {
-		time.Sleep(time.Millisecond * 10)
-		num, ok := <-intChan
+		v, ok := <-ch
 		if !ok {
 			break
 		}
-		flag = true
-		for i := 2; i < num; i++ {
-			if num%i == 0 {
-				flag = false
-				break
-			}
-		}
-		if flag {
-			primeChan <- num
-		}
+		fmt.Println(v)
 	}
-	fmt.Println("有一個 primeNum 協程因为取不到數據，退出")
-	exitChan <- true
+	var a struct{}
+	exitChan <- a
 }
 func main() {
-	intChan := make(chan int, 1000)
-	primeChan := make(chan int, 20000)
-	exitChan := make(chan bool, 4)
-	go putNum(intChan)
-	for i := 0; i < 4; i++ {
-		go primeNum(intChan, primeChan, exitChan)
-	}
-	go func() {
-		for i := 0; i < 4; i++ {
-			<-exitChan
-		}
-		close(primeChan)
-	}()
-	for {
-		res, ok := <-primeChan
-		if !ok {
+	ch := make(chan int, 10)
+	exitChan := make(chan struct{}, 2)
+	go send(ch, exitChan)
+	go recv(ch, exitChan)
+	var total = 0
+	for _ = range exitChan {
+		total++
+		if total == 2 {
 			break
 		}
-		fmt.Println("質數 =", res)
 	}
-	fmt.Println("主線程退出")
+	fmt.Println("结束")
 }
