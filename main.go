e6a0389b6985654a46861403e2be9c88c6f62933
jiro <jiro853141@gmail.com>
2024-01-18 22:13:30 +0800

    281

--- main.go
+++ main.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	intChan := make(chan int, 10)
+func sayHello() {
 	for i := 0; i < 10; i++ {
-		intChan <- i
+		time.Sleep(time.Second)
+		fmt.Println("Hello World")
 	}
-	stringsChan := make(chan string, 5)
-	for i := 0; i < 5; i++ {
-		stringsChan <- "hello" + fmt.Sprintf("%d", i)
-	}
-	for {
-		select {
-		case v := <-intChan:
-			fmt.Printf("從 intChan 讀取的數據 %d\n", v)
-		case v := <-stringsChan:
-			fmt.Printf("從 stringsChan讀取的數據 %s\n", v)
-		default:
-			fmt.Printf("都取不到了\n")
-			return
+}
+func test() {
+	// 可以使用 defer + recover
+	defer func() {
+		// 獲取 test 抛出的 panic
+		if err := recover(); err != nil {
+			fmt.Println("test() 发生错误", err)
 		}
+	}()
+	var myMap map[int]string
+	myMap[0] = "golang" // error
+}
+func main() {
+	go sayHello()
+	go test()
+	for i := 0; i < 10; i++ {
+		fmt.Println("main() ok =", i)
+		time.Sleep(time.Second)
 	}
 }
