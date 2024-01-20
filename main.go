221f889e5d52b1dfbff8f51d5f9c6a4fdabffd3c
jiro <jiro853141@gmail.com>
2024-01-20 23:30:52 +0800

    285

--- main.go
+++ main.go
@@ -2,31 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"reflect"
 )
 
-func sayHello() {
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Second)
-		fmt.Println("Hello World")
-	}
-}
-func test() {
-	// 可以使用 defer + recover
-	defer func() {
-		// 獲取 test 抛出的 panic
-		if err := recover(); err != nil {
-			fmt.Println("test() 发生错误", err)
-		}
-	}()
-	var myMap map[int]string
-	myMap[0] = "golang" // error
+func reflectTest01(b interface{}) {
+	rType := reflect.TypeOf(b)
+	fmt.Println("rType =", rType)
+	rVal := reflect.ValueOf(b)
+	fmt.Printf("rVal = %v, rVal type = %T\n", rVal, rVal)
+	n2 := 2 + rVal.Int()
+	fmt.Println("n2 =", n2)
+	iV := rVal.Interface()
+	num2 := iV.(int)
+	fmt.Println("num2 =", num2)
 }
 func main() {
-	go sayHello()
-	go test()
-	for i := 0; i < 10; i++ {
-		fmt.Println("main() ok =", i)
-		time.Sleep(time.Second)
-	}
+	var num int = 100
+	reflectTest01(num)
 }
