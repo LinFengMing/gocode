ad9d185f40c7c83963a715cd76f466ab18f80e9d
jiro <jiro853141@gmail.com>
2024-01-21 15:56:32 +0800

    288

--- main.go
+++ main.go
@@ -5,41 +5,13 @@ import (
 	"reflect"
 )
 
-type Student struct {
-	Name string
-	Age  int
-}
-
-func reflectTest01(b interface{}) {
-	rType := reflect.TypeOf(b)
-	fmt.Println("rType =", rType)
+func reflect01(b interface{}) {
 	rVal := reflect.ValueOf(b)
-	fmt.Printf("rVal = %v, rVal type = %T\n", rVal, rVal)
-	n2 := 2 + rVal.Int()
-	fmt.Println("n2 =", n2)
-	iV := rVal.Interface()
-	num2 := iV.(int)
-	fmt.Println("num2 =", num2)
-}
-func reflectTest02(b interface{}) {
-	rType := reflect.TypeOf(b)
-	fmt.Println("rType =", rType)
-	rVal := reflect.ValueOf(b)
-	fmt.Printf("rVal = %v, rVal type = %T\n", rVal, rVal)
-	fmt.Printf("kind = %v, kind = %v\n", rType.Kind(), rVal.Kind())
-	iV := rVal.Interface()
-	fmt.Printf("iV = %v, iV type = %T\n", iV, iV)
-	stu, ok := iV.(Student)
-	if ok {
-		fmt.Printf("stu.Name = %v\n", stu.Name)
-	}
+	fmt.Println("rVal kind =", rVal.Kind())
+	rVal.Elem().SetInt(20)
 }
 func main() {
 	var num int = 100
-	reflectTest01(num)
-	stu := Student{
-		Name: "tom",
-		Age:  20,
-	}
-	reflectTest02(stu)
+	reflect01(&num)
+	fmt.Println("num =", num)
 }
