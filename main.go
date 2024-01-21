91f9bc9ea7ed5ca71bbfd00aa8edce0f3e3085e4
jiro <jiro853141@gmail.com>
2024-01-21 22:35:53 +0800

    290

--- main.go
+++ main.go
@@ -5,9 +5,59 @@ import (
 	"reflect"
 )
 
+type Monster struct {
+	Name  string  `json:"name"`
+	Age   int     `json:"monster_age"`
+	Score float32 `json:"score"`
+	Sex   string
+}
+
+func (s Monster) Print() {
+	fmt.Println("---start---")
+	fmt.Println(s)
+	fmt.Println("---end---")
+}
+func (s Monster) GetSum(n1, n2 int) int {
+	return n1 + n2
+}
+func (s Monster) Set(name string, age int, score float32, sex string) {
+	s.Name = name
+	s.Age = age
+	s.Score = score
+	s.Sex = sex
+}
+func TestStruct(a interface{}) {
+	typ := reflect.TypeOf(a)
+	val := reflect.ValueOf(a)
+	kd := val.Kind()
+	if kd != reflect.Struct {
+		fmt.Println("expect struct")
+		return
+	}
+	num := val.NumField()
+	fmt.Printf("struct has %d fields\n", num)
+	for i := 0; i < num; i++ {
+		fmt.Printf("Field %d: value = %v\n", i, val.Field(i))
+		tagVal := typ.Field(i).Tag.Get("json")
+		if tagVal != "" {
+			fmt.Printf("Field %d: tag = %v\n", i, tagVal)
+		}
+	}
+	numOfMethod := val.NumMethod()
+	fmt.Printf("struct has %d methods\n", numOfMethod)
+	// 方法的排序默認是按照函數名的排序（ASCII碼）
+	val.Method(1).Call(nil)
+	var params []reflect.Value
+	params = append(params, reflect.ValueOf(10))
+	params = append(params, reflect.ValueOf(40))
+	res := val.Method(0).Call(params) // 傳入的參數是 []reflect.Value, 返回 []reflect.Value
+	fmt.Println("res=", res[0].Int())
+}
 func main() {
-	var str string = "tom"
-	fs := reflect.ValueOf(&str)
-	fs.Elem().SetString("jack")
-	fmt.Println(str)
+	var a Monster = Monster{
+		Name:  "牛魔王",
+		Age:   500,
+		Score: 98.8,
+	}
+	TestStruct(a)
 }
