108bfeac155a95f068fe67d7dfae074a9d6c3cd4
jiro <jiro853141@gmail.com>
2024-01-22 23:00:32 +0800

    296

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("伺服器開始監聽")
+	listen, err := net.Listen("tcp", "0.0.0.0:8888")
+	if err != nil {
+		fmt.Println("listen error =", err)
+		return
+	}
+	defer listen.Close()
+	for {
+		fmt.Println("等待用戶端連接")
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Accept error =", err)
+		} else {
+			fmt.Printf("Accept success conn = %v, ip = %v\n", conn, conn.RemoteAddr().String())
+		}
+	}
+}
