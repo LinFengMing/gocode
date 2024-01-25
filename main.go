4e15b019c44ca04b7c1651dfefd8188b3e7c2c12
jiro <jiro853141@gmail.com>
2024-01-26 00:39:29 +0800

    309

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	fmt.Println(rdb)
+}
