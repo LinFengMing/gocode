48883f8c3254349a283ad3b7167dcdfb02d0700a
jiro <jiro853141@gmail.com>
2024-01-26 23:28:34 +0800

    311

--- main.go
+++ main.go
@@ -15,15 +15,49 @@ func main() {
 	})
 	defer rdb.Close()
 	fmt.Println(rdb)
-	err := rdb.Set(context.Background(), "score", 100, 0).Err()
+	err := rdb.HSet(context.Background(), "user01", "name", "john").Err()
 	if err != nil {
-		fmt.Printf("set score failed, err:%v\n", err)
+		fmt.Println("Error setting hash name:", err)
 		return
 	}
-	val, err := rdb.Get(context.Background(), "score").Result()
+	err = rdb.HSet(context.Background(), "user01", "age", 10).Err()
 	if err != nil {
-		fmt.Printf("get score failed, err:%v\n", err)
+		fmt.Println("Error setting hash age:", err)
 		return
 	}
-	fmt.Println("score", val)
+	result, err := rdb.HGet(context.Background(), "user01", "name").Result()
+	if err == redis.Nil {
+		fmt.Println("Field not found in the hash.")
+	} else if err != nil {
+		fmt.Println("Error getting hash name:", err)
+	} else {
+		fmt.Printf("Value of %s in %s: %s\n", "name", "user01", result)
+	}
+	result2, err := rdb.HGet(context.Background(), "user01", "age").Result()
+	if err == redis.Nil {
+		fmt.Println("Field not found in the hash.")
+	} else if err != nil {
+		fmt.Println("Error getting hash age:", err)
+	} else {
+		fmt.Printf("Value of %s in %s: %s\n", "age", "user01", result2)
+	}
+	fieldsValues := map[string]interface{}{
+		"name": "tom",
+		"age":  18,
+	}
+	err = rdb.HMSet(context.Background(), "user02", fieldsValues).Err()
+	if err != nil {
+		fmt.Println("Error setting multiple hash fields:", err)
+		return
+	}
+	fieldsToGet := []string{"name", "age"}
+	results, err := rdb.HMGet(context.Background(), "user02", fieldsToGet...).Result()
+	if err != nil {
+		fmt.Println("Error getting multiple hash fields:", err)
+		return
+	}
+
+	for i, field := range fieldsToGet {
+		fmt.Printf("Value of %s in %s: %v\n", field, "user02", results[i])
+	}
 }
