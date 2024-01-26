8aaa8b0155abcefe956a86ef6b9681f7bcf10753
jiro <jiro853141@gmail.com>
2024-01-26 23:00:47 +0800

    310

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/redis/go-redis/v9"
@@ -12,5 +13,17 @@ func main() {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+	defer rdb.Close()
 	fmt.Println(rdb)
+	err := rdb.Set(context.Background(), "score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed, err:%v\n", err)
+		return
+	}
+	val, err := rdb.Get(context.Background(), "score").Result()
+	if err != nil {
+		fmt.Printf("get score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
 }
