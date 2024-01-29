924164c906119de9c7196fb9925748198325fa71
jiro <jiro853141@gmail.com>
2024-01-29 17:26:11 +0800

    314

---
+++ client/login.go
@@ -0,0 +1,8 @@
+package client
+
+import "fmt"
+
+func Login(userId int, userPwd string) (err error) {
+	fmt.Printf("userId = %d userPwd = %s\n", userId, userPwd)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
-
-	"github.com/redis/go-redis/v9"
+	"gocode/client"
 )
 
+var userId int
+var userPwd string
+
 func main() {
-	pool := redis.NewClient(&redis.Options{
-		Addr:         "127.0.0.1:6379",
-		Password:     "", // no password set
-		DB:           0,  // use default DB
-		PoolSize:     10,
-		MinIdleConns: 5,
-		DialTimeout:  5 * time.Minute,
-	})
-	fmt.Println(pool)
-	defer pool.Close()
-	setErr := pool.Set(context.Background(), "name", "tom cat", 0)
-	if setErr != nil {
-		fmt.Println(setErr)
+	var key int
+	var loop bool = true
+	for loop {
+		fmt.Println("----------歡迎登入多人聊天系統----------")
+		fmt.Println("\t\t\t 1 登入聊天室")
+		fmt.Println("\t\t\t 2 註冊用戶")
+		fmt.Println("\t\t\t 3 退出系統")
+		fmt.Println("\t\t\t 請選擇(1-3):")
+		fmt.Scanf("%d\n", &key)
+		switch key {
+		case 1:
+			fmt.Println("登入聊天室")
+			loop = false
+		case 2:
+			fmt.Println("註冊用戶")
+			loop = false
+		case 3:
+			fmt.Println("退出系統")
+			loop = false
+		default:
+			fmt.Println("輸入錯誤，請重新輸入")
+		}
 	}
-	val, getErr := pool.Get(context.Background(), "name").Result()
-	if getErr != nil {
-		fmt.Println(getErr)
+	if key == 1 {
+		// 進行用戶登入
+		fmt.Println("請輸入用戶的ID:")
+		fmt.Scanf("%d\n", &userId)
+		fmt.Println("請輸入用戶的密碼:")
+		fmt.Scanf("%s\n", &userPwd)
+		err := client.Login(userId, userPwd)
+		if err != nil {
+			fmt.Println("登入失敗")
+		} else {
+			fmt.Println("登入成功")
+		}
+	} else if key == 2 {
+		fmt.Println("註冊用戶的流程")
+	} else {
+		fmt.Println("退出系統")
 	}
-	fmt.Println(val)
 }
